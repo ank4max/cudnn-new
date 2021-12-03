de9b73082786d9734f6d1b9e32db350b52b9c6e1
aditya9706 <41884965+aditya9706@users.noreply.github.com>
2021-12-03 13:34:21 +0530

    Update run_cudnn.py

--- run_cudnn.py
+++ run_cudnn.py
@@ -1,10 +1,15 @@
 import glob
 import os
-import subprocess
+import sys
 
+// utility - 
+args = {}
+for i in len(sys.args):
+  if(sys.args[i] == "-testfile"):
+    args["-testfile"] = sys.args[i + 1]
 list_executable = glob.glob("Executables/*")
 
-f = open("cudnn.test", "r")
+f = open(args["-testfile"], "r")
 
 cmd_args = {}
 for l in f :
@@ -21,4 +26,3 @@ print(list_executable)
 for exe in list_executable :
   print(exe + " " + cmd_args[exe.split("/")[1]])
   os.system(exe + " " + cmd_args[exe.split("/")[1]])
-  # subprocess.check_output("./" + exe + " " + cmd_args[exe.split("/")[1]])
