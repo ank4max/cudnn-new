0a399058939e2c67a9b57549f230ac5c213b22a5
ank4max <95414875+ank4max@users.noreply.github.com>
2021-12-16 19:19:56 +0530

    Update cublasdot.cpp

--- workspace/cublasdot.cpp
+++ workspace/cublasdot.cpp
@@ -4,48 +4,66 @@
 # include "cublas_v2.h"
 
 int main ( int argc,char **argv ) {
-cudaError_t cudaStat ; // cudaMalloc status
-cublasStatus_t stat ; // CUBLAS functions status
-cublasHandle_t handle ;
-int n= stoi(argv[1]);
-int j;                  // index of elements
-float * x;             // n- vector on the host
-float * y;             // n- vector on the host
-x = ( float *) malloc (n* sizeof (*x)); // host memory alloc for x
 
-for(j = 0;j < n;j++) {
-x[j] = ( float )j; // x={0 ,1 ,2 ,3 ,4 ,5}
-}
-y = ( float *) malloc (n* sizeof (*y)); // host memory alloc for y
-for(j = 0;j < n;j++) {
-y[j]=( float )j; // y={0 ,1 ,2 ,3 ,4 ,5}
-}
-printf ("x,y:\n");
-for(j=0;j<n;j++)
-printf (" %2.0f,",x[j]); // print x,y
-printf ("\n");
-// on the device
-float * d_x; // d_x - x on the device
-float * d_y; // d_y - y on the device
-cudaStat = cudaMalloc (( void **)& d_x ,n* sizeof (*x)); // device
-// memory alloc for x
-cudaStat = cudaMalloc (( void **)& d_y ,n* sizeof (*y)); // device
-// memory alloc for y
-stat = cublasCreate (& handle ); // initialize CUBLAS context
-stat = cublasSetVector (n, sizeof (*x) ,x ,1 ,d_x ,1); // cp x- >d_x
-stat = cublasSetVector (n, sizeof (*y) ,y ,1 ,d_y ,1); // cp y- >d_y
-float result ;
-// dot product of two vectors d_x ,d_y :
-// d_x [0]* d_y [0]+...+ d_x [n -1]* d_y [n -1]
-stat=cublasSdot(handle,n,d x,1,d y,1,&result);
-printf ("dot product x.y:\n");
-printf (" %7.0 f\n",result ); // print the result
-cudaFree (d_x ); // free device memory
-cudaFree (d_y ); // free device memory
-cublasDestroy ( handle ); // destroy CUBLAS context
-free (x); // free host memory
-free (y); // free host memory
-return EXIT_SUCCESS ;
+  //initializing size of vector with command line arguement
+  cudaError_t cudaStat ; 
+  cublasStatus_t stat ; 
+  cublasHandle_t handle ;
+  int n= atoi(argv[1]);
+  int j; 
+  
+  //pointers x and y pointing  to vectors
+  float * x;             
+  float * y; 
+  
+  //host memory allocation for vectors
+  x = ( float *) malloc (n* sizeof (*x)); 
+  y = ( float *) malloc (n* sizeof (*y)); 
+
+  //setting up values in x and y vectors
+  for(j = 0;j < n; j++) {
+    x[j] = ( float )j; // x={0 ,1 ,2 ,3 ,4 ,5}
+  }
+
+  for (j = 0; j < n; j++) {
+    y[j] = ( float )j; 
+  }
+  
+  //printing the initial values in vector x which is same as y
+  printf ("x,y:\n");
+  for (j = 0; j < n; j++) {
+    printf (" %2.0f,",x[j]); 
+  }
+  printf ("\n");
+  
+  // Pointers for device memory allocation
+  float * d_x; 
+  float * d_y; 
+  cudaStat = cudaMalloc (( void **)& d_x, n* sizeof (*x)); 
+  cudaStat = cudaMalloc (( void **)& d_y, n* sizeof (*y)); 
+ 
+  //initializing cublas library and setting up values for vectors in device memory same values as that present in host vectors 
+  stat = cublasCreate (& handle );
+  stat = cublasSetVector (n, sizeof (*x) ,x ,1 ,d_x ,1); 
+  stat = cublasSetVector (n, sizeof (*y) ,y ,1 ,d_y ,1); 
+
+  float result ;
+  // performing dot product operation and storing result in result variable
+  stat=cublasSdot(handle, n, d_x, 1, d_y, 1, &result);
+  
+  //printing the final result
+  printf ("dot product x.y:\n");
+  printf (" %7.0 f\n",result ); 
+  
+  //freeing device memory
+  cudaFree (d_x ); 
+  cudaFree (d_y );
+  
+  //destroying cublas context and freeing host memory
+  cublasDestroy ( handle ); 
+  free (x); 
+  free (y); 
+  return EXIT_SUCCESS ;
 }
 // x,y:
 // 0 , 1 , 2 , 3 , 4 , 5 ,
