1ba3b0a303c918519268dc03b83d1cb228ea21c5
ank4max <95414875+ank4max@users.noreply.github.com>
2021-12-22 14:45:28 +0530

    Update run_cub.py

--- workspace/run_cub.py
+++ workspace/run_cub.py
@@ -1,21 +1,23 @@
 '''
 /* Copyright 2021-2022 Enflame. All Rights Reserved.
  *
- * @file    run_cudnn.py
+ * @file    run_cublas.py
  * @brief   To run benchmarking test for cudnn API
  *
  * @author  ashish(CAI)
- * @date    2021-12-13
+ * @date    2021-12-17
  * @version V1.0
  * @par     Copyright (c)
  *          Enflame Tech Company.
  * @par     History:
  */
 '''
-
 import glob
 import os 
 import argparse
+import subprocess
+import tabulate
+from datetime import datetime
 
 # creating object files 
 list_cpp = glob.glob("*.cpp")
@@ -39,8 +41,69 @@ args = vars(ap.parse_args())
 cfg_path = args['configfile']
 config = open(cfg_path, "r")
 
+Table = []
+commands = []
+total_cases = 0
+passed_cases = 0
+
 # Running executables            
 for cmd in config:
-  print(cmd)  
+  commands.append(cmd)
+  cmd = "./Executables/" + cmd
   os.system("cd Executables")
-  os.system("./Executables/" + cmd) 
+  total_cases += 1
+  
+  output = []
+  status = ""
+  lines = []
+  if (os.system(cmd + " >> output.txt") == 0):
+    status = "PASSED"
+    passed_cases += 1
+    proc = subprocess.Popen([cmd], stdout = subprocess.PIPE, shell = True)
+    (out, err) = proc.communicate()
+    output = str(out).split("\\n")
+
+
+  else :
+    status = "FAILED"
+
+  summary = {"cuBLAS API": "", "Latency": "", "Throughput": "", "Test Level": "", "Status": status}
+
+  arguments = cmd.split(" ")
+  for line in arguments :
+    if ("./" in line) :
+      executable = line.split("/")[2]
+      summary["cuBLAS API"] = executable.split("_")[1]
+    elif ("-L" in line) :
+      summary["Test Level"] = line.split("-")[1]
+
+  for line in output :
+    if ("Latency" in line) :
+      summary["Latency"] = line.split(": ")[1]
+    elif ("Throughput" in line) :
+      summary["Throughput"] = line.split(": ")[1]
+  Table.append(summary)
+
+
+print("\n\nExecuted below cuBLAS Test Cases")
+print("===============================")
+for cmd in commands :
+  print(cmd.split("\n")[0])
+
+# Printing table
+print("\n\n\n=============================")
+print("Test Result Summary of cuBLAS")
+print("=============================")
+print("Executed on: ", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+header = Table[0].keys()
+rows =  [x.values() for x in Table]
+print("\n")
+print(tabulate.tabulate(rows, header))
+
+failed_cases = total_cases - passed_cases
+passed_percentage = (passed_cases * 100) / total_cases
+
+print("\n\n[{passed}/{total} PASSED]".format(passed = passed_cases, total = total_cases))
+print("{percent}% tests passed, {failed} tests failed out of {total}".format(percent = passed_percentage, failed = failed_cases, total = total_cases))
+
