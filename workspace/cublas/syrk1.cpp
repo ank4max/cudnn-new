fe427250f1a2e3cf965991f2be616e4c45830d24
ank4max <95414875+ank4max@users.noreply.github.com>
2021-12-30 10:27:37 +0530

    Update syrk1.cpp

--- workspace/cublas/syrk1.cpp
+++ workspace/cublas/syrk1.cpp
@@ -2,12 +2,68 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 # include "cublas_v2.h"
+#define index(i,j,ld) (((j)*(ld))+(i))
 
-# define IDX2C(i ,j , ld ) ((( j )*( ld ))+( i ))
+
+void PrintMat(float* PrintMatrix, int col, int row) {
+  int i, j;
+  for (i = 0; i < row; i++) {
+    std::cout << "\n";
+    for (j = 0; j < col; j++) {
+      std::cout << PrintMatrix[index(i, j, row)] << " ";
+    }
+  }
+  std::cout << "\n";
+}
+
+char* SubStr(char* InputArr, int begin, int len) {
+  char* ResultStr = new char[len + 1];
+  for (int i = 0; i < len; i++) {
+    ResultStr[i] = *(InputArr + begin + i);
+  }
+  ResultStr[len] = 0;
+  return ResultStr;
+}
+
+
+
+
+
+
+
 # define n 6 // a - nxk matrix
 # define k 4 // c - nxn matrix
 
-int main ( void ) {
+int main (int argc, char **argv) {
+  int x_row, x_col, y_row, y_col;
+  float alpha, beta;
+  for (int i = 0; i < argc; i++) {
+    std::cout << argv[i] << std::endl;
+  }
+  
+  for (int i = 1; i < 7; i++) {
+    int len = sizeof(argv[i]);
+    if (!strcmp(SubStr(argv[i], 1, 5), "x_row"))
+      x_row = atoi(argv[i] + 6);
+    else if (!strcmp(SubStr(argv[i], 1, 5), "x_col"))
+      x_col = atoi(argv[i] + 6);
+    else if (!strcmp(SubStr(argv[i], 1, 5), "y_row"))
+      y_row = atoi(argv[i] + 6);
+    else if (!strcmp(SubStr(argv[i], 1, 5), "y_col"))
+      y_col = atoi(argv[i] + 6);
+    else if (!strcmp(SubStr(argv[i], 1, 5), "alpha"))
+      alpha = atof(argv[i] + 6);
+     else if (!strcmp(SubStr(argv[i], 1, 4), "beta"))
+      beta = atof(argv[i] + 5);
+  }
+  
+  if (x_row != y_row) {
+    return EXIT_FAILURE;
+  }
+  if (y_row != y_col) {
+    return EXIT_FAILURE;
+  }
+  
   cudaError_t cudaStatus ; 
   cublasStatus_t status ; 
   cublasHandle_t handle ; 
@@ -15,98 +71,141 @@ int main ( void ) {
   
   float *HostMatX;                   // nxk matrix a on the host
   float *HostMatY;                   // nxn matrix c on the host
-  HostMatX = (float *) malloc (n*k* sizeof (float)); // host memory for a
-  HostMatY = (float *) malloc (n*n* sizeof (float)); // host memory for c
+  HostMatX = (float *) malloc (x_row * x_col * sizeof (float)); // host memory for a
+  HostMatY = (float *) malloc (y_row * y_col * sizeof (float)); // host memory for c
   
   if (HostMatX == 0) {
-    fprintf (stderr, "!!!! host memory allocation error (matrixX)\n");
+    fprintf (stderr, "!!!! Host memory allocation error (matrixX)\n");
     return EXIT_FAILURE;
   }
   if (HostMatY == 0) {
-    fprintf (stderr, "!!!! host memory allocation error (matrixY)\n");
+    fprintf (stderr, "!!!! Host memory allocation error (matrixY)\n");
     return EXIT_FAILURE;
   }
   
   // define the lower triangle of an nxn symmetric matrix c
   // column by column
   int ind =11; // c:
-  for(j = 0; j < n; j++) { // 11
-    for(i = 0; i < n; i++) { // 12 ,17
-      if(i >= j) { // 13 ,18 ,22
-        HostMatY[ IDX2C (i,j,n )] = (float)ind ++; // 14 ,19 ,23 ,26
-      } // 15 ,20 ,24 ,27 ,29
-    } // 16 ,21 ,25 ,28 ,30 ,31
+  for(j = 0; j < y_col; j++) {                                  // 11
+    for(i = 0; i < y_row; i++) {                                / / 12 ,17
+      if(i >= j) {                                           // 13 ,18 ,22
+        HostMatY[index(i, j, y_row)] = (float)ind ++;              // 14 ,19 ,23 ,26
+      }                                                     // 15 ,20 ,24 ,27 ,29
+    }                                                      // 16 ,21 ,25 ,28 ,30 ,31
   }
   
   // print the lower triangle of c row by row
   printf (" lower triangle of c:\n");
-  for(i = 0; i < n; i++) {
-    for(j = 0; j < n; j++) {
+  for(i = 0; i < y_row; i++) {
+    for(j = 0; j < y_col; j++) {
       if(i >= j) {
-        printf (" %5.0f",HostMatY[ IDX2C (i,j,n )]);
+        std::cout << HostMatY[index(i, j, y_row)] << " ";
       }
     }
-    printf ("\n");
+    std::cout << "\n";
   }
   
   // define an nxk matrix a column by column
   ind =11; // a:
-  for(j = 0; j < k; j++) { // 11 ,17 ,23 ,29
-    for(i = 0; i < n; i++) { // 12 ,18 ,24 ,30
-      HostMatX[ IDX2C (i,j,n )] = ( float )ind; // 13 ,19 ,25 ,31
-      ind ++; // 14 ,20 ,26 ,32
-    } // 15 ,21 ,27 ,33
-  } // 16 ,22 ,28 ,34
+  for(j = 0; j < x_col; j++) {                          // 11 ,17 ,23 ,29
+    for(i = 0; i < x_row; i++) {                        // 12 ,18 ,24 ,30
+      HostMatX[index(i, j, x_row)] = (float)ind;        // 13 ,19 ,25 ,31
+      ind ++;                                       // 14 ,20 ,26 ,32
+    }                                               // 15 ,21 ,27 ,33
+  }                                                 // 16 ,22 ,28 ,34
 
-  printf ("a:\n");
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < k; j++) {
-      printf (" %5.0f",HostMatX[ IDX2C (i,j,n )]); // print a row by row
-    }
-    printf ("\n");
-  }
+  std::cout << "\nMatriz X:";
+  PrintMat(HostMatX, x_col, x_row);
   
   // on the device
   float * DeviceMatX; // d_a - a on the device
   float * DeviceMatY; // d_c - c on the device
 
   
-  cudaStatus = cudaMalloc((void **)& DeviceMatX, n * k * sizeof (*HostMatX)); // device
+  cudaStatus = cudaMalloc((void **)& DeviceMatX, x_row * x_col * sizeof (*HostMatX)); // device
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for X\n";
     return EXIT_FAILURE;
   }
   // memory alloc for a
-  cudaStatus = cudaMalloc((void **)& DeviceMatY, n*n* sizeof (*HostMatY)); // device
+  cudaStatus = cudaMalloc((void **)& DeviceMatY, y_row * y_col * sizeof (*HostMatY)); // device
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for Y\n";
     return EXIT_FAILURE;
   }
   // memory alloc for c
   status = cublasCreate (& handle); // initialize CUBLAS context
-// copy matrices from the host to the device
-stat = cublasSetMatrix (n,k, sizeof (*a) ,a,n,d_a ,n); //a -> d_a
-stat = cublasSetMatrix (n,n, sizeof (*c) ,c,n,d_c ,n); //c -> d_c
-float al =1.0f; // al =1
-float bet =1.0f; // bet =1
-// symmetric rank -k update : c = al*d_a *d_a ^T + bet *d_c ;
-// d_c - symmetric nxn matrix , d_a - general nxk matrix ;
-// al ,bet - scalars
-stat=cublasSsyrk(handle,CUBLAS_FILL_MODE_LOWER,CUBLAS_OP_N,
-n,k,&al,d_a,n,&bet,d_c,n);
-stat = cublasGetMatrix (n,n, sizeof (*c) ,d_c ,n,c,n); // d_c -> c
-printf (" lower triangle of updated c after Ssyrk :\n");
-for(i=0;i<n;i ++){
-for(j=0;j<n;j ++){
-if(i >=j) // print the lower triangle
-printf (" %7.0f",c[ IDX2C (i,j,n )]); // of c after Ssyrk
-}
-printf ("\n");
-}
-cudaFree (d_a ); // free device memory
-cudaFree (d_c ); // free device memory
-cublasDestroy ( handle ); // destroy CUBLAS context
-free (a); // free host memory
-free (c); // free host memory
-return EXIT_SUCCESS ;
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Failed to initialize handle\n");
+    return EXIT_FAILURE;
+  }
+  // copy matrices from the host to the device
+  status = cublasSetMatrix (x_row, x_col, sizeof (*HostMatX), HostMatX, x_row, DeviceMatX, x_row); //a -> d_a
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix X from host to device failed \n");
+    return EXIT_FAILURE;
+  } 
+  status = cublasSetMatrix (y_row, y_col, sizeof (*HostMatY), HostMatX, y_row, DeviceMatY, y_row); //c -> d_c
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix Y from host to device failed \n");
+    return EXIT_FAILURE;
+  }
+  
+  // symmetric rank -k update : c = al*d_a *d_a ^T + bet *d_c ;
+  // d_c - symmetric nxn matrix , d_a - general nxk matrix ;
+  // al ,bet - scalars
+  
+  
+  start = clock();
+  
+  status = cublasSsyrk(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+  x_row, x_col, &alpha, DeviceMatX, x_row, &beta, DeviceMatY, y_row);
+  
+  end = clock();
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! kernel execution error\n");
+    return EXIT_FAILURE;
+  }
+  
+  
+  status = cublasGetMatrix (y_row, y_col, sizeof (*HostMatY), DeviceMatY, y_row, HostMatY, y_row); // d_c -> c
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to get output matrix Y from device\n");
+    return EXIT_FAILURE;
+  }
+  
+  printf (" lower triangle of updated c after Ssyrk :\n");
+  for(i = 0; i < y_row; i++) {
+    for(j = 0; j < y_col; j++) {
+      if(i >=j) {  // print the lower triangle
+        std::cout << [index(i, j, y_row)] << " " ;  // of c after Ssyrk
+      }
+    }
+    printf ("\n");
+  }
+  
+  // printing latency and throughput of the function
+  std::cout << "\nLatency: " <<  ((double)(end - start)) / double(CLOCKS_PER_SEC) <<
+        "\nThroughput: " << (1e-9 * 2) / (end - start) << "\n\n";
+  
+  
+  cudaStatus = cudaFree (DeviceMatX); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for X" << std::endl;
+    return EXIT_FAILURE;   
+  }
+  cudaStatus = cudaFree (DeviceMatY); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for Y" << std::endl;
+    return EXIT_FAILURE;   
+  }
+  
+  status = cublasDestroy (handle); // destroy CUBLAS context
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to uninitialize handle \n");
+    return EXIT_FAILURE;
+  }
+  free (HostMatX); // free host memory
+  free (HostMatY); // free host memory
+  return EXIT_SUCCESS ;
 }
