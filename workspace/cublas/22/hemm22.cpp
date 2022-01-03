95a7d5c3d70f1b1bb8b9d3ec0aa65bd164b27766
ank4max <95414875+ank4max@users.noreply.github.com>
2022-01-03 19:15:08 +0530

    Create hemm22.cpp

---
+++ workspace/cublas/22/hemm22.cpp
@@ -0,0 +1,236 @@
+# include <iostream>
+# include <stdlib.h>
+# include <cuda_runtime.h>
+# include "cublas_v2.h"
+# include <string>
+#define INDEX(row, col, row_count) (((col)*(row_count))+(row))   // for getting index values matrices
+#define THROUGHPUT(clk_start, clk_end)  ((1e-9 * 2) / (clk_end - clk_start)) 
+#define RANDOM (rand() % 10000 * 1.00) / 100
+
+int main (int argc, char **argv) {
+  
+  cudaError_t cudaStatus ; // cudaMalloc status
+  cublasStatus_t status ; // CUBLAS functions status
+  cublasHandle_t handle ; // CUBLAS context
+  int row, col; // i-row index , j-col. ind.
+  int A_row, A_col, B_row, B_col, C_row, C_col ;
+  float alpha_real, alpha_imaginary, beta_real, beta_imaginary;
+
+  std::cout << argv[0] << std::endl;
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::cout << argv[loop_count] << " ";
+    if(loop_count + 1 < argc)
+      std::cout << argv[loop_count + 1] << std::endl;
+  }
+  std::cout << std::endl;
+  
+  // reading cmd line arguments
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::string cmd_argument(argv[loop_count]);  
+    if (!(cmd_argument.compare("-A_row")))
+      A_row = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-B_column")))
+      B_col = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-alpha_real")))
+      alpha_real = atof(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-beta_real")))
+      beta_real = atof(argv[loop_count + 1]);
+  }
+  
+  A_col = A_row;
+  B_row = A_col;
+  C_row = A_row;
+  C_col = B_col;
+  alpha_imaginary =0.0f;
+  beta_imaginary = 0.0f;
+  
+  time_t clk_start, clk_end;
+  // data preparation on the host
+  cuComplex *HostMatA; // mxm complex matrix a on the host
+  cuComplex *HostMatB; // mxn complex matrix b on the host
+  cuComplex *HostMatC; // mxn complex matrix c on the host
+  HostMatA = new cuComplex[A_row * A_col]; // host memory
+  // alloc for x
+  HostMatB = new cuComplex[B_row * B_col]; // host memory
+  // alloc for b
+  HostMatC = new cuComplex[C_row * C_col]; // host memory
+  // alloc for c
+  
+  if (HostMatA == 0) {
+    fprintf (stderr, "!!!! host memory allocation error (matrix X)\n");
+    return EXIT_FAILURE;
+  }
+  if (HostMatB == 0) {
+    fprintf (stderr, "!!!! host memory allocation error (matrix Y)\n");
+    return EXIT_FAILURE;
+  }
+  if (HostMatC == 0) {
+    fprintf (stderr, "!!!! host memory allocation error (matrix Z)\n");
+    return EXIT_FAILURE;
+  }
+  
+  
+  // define the lower triangle of an mxm Hermitian matrix a in
+  // lower mode column by column
+  int ind =11; // a:
+  for (col = 0; col < A_col; col++) {                 // 11
+    for (row = 0; row < A_row; row++) {                                   // 12 ,17
+      if(row >= col) {                                        // 13 ,18 ,22
+        HostMatA[INDEX(row, col, A_row)].x = (float)ind ++;                   // 14 ,19 ,23 ,26
+        HostMatA[INDEX(row, col, A_row)].y = 0.0f;                       // 15 ,20 ,24 ,27 ,29
+      }                                                           // 16 ,21 ,25 ,28 ,30 ,31
+    }
+  }
+  // print the lower triangle of a row by row
+  std::cout << " lower triangle of X :\n" ;
+  for (row = 0; row < A_row; row++){
+    for (col = 0; col < A_col; col++) {
+      if(row >= col) {
+        std::cout << HostMatA[INDEX(row, col, A_row)].x << "+" << HostMatA[INDEX(row, col, A_row)].y << "*I "    ;                              
+      }
+    }
+  std::cout << "\n";
+  }
+  // define mxn matrices b,c column by column
+  ind =11; // b,c:
+  for(col = 0; col < B_col; col++) {           // 11 ,17 ,23 ,29 ,35
+    for(row = 0; row < B_row; row++) {                      // 12 ,18 ,24 ,30 ,36
+      HostMatB[INDEX(row, col, B_row)].x = (float)ind;            // 13 ,19 ,25 ,31 ,37
+      HostMatB[INDEX(row, col, B_row)].y =0.0f;                   // 14 ,20 ,26 ,32 ,38
+                   
+      ind ++;
+    }
+  }
+  
+  // define mxn matrices c column by column
+  ind =11; 
+  for(col = 0; col < C_col; col++) {           
+    for(row = 0; row < C_row; row++) {                      
+      HostMatC[INDEX(row, col, C_row)].x = (float)ind;              
+      HostMatC[INDEX(row, col, C_row)].y = 0.0f;             
+      ind ++;
+    }
+  }
+ // print b(=c) row by row
+  std::cout << "b,c:\n";
+  for (row = 0; row < C_row; row++) {
+    for (col = 0; col < C_col; col++) {
+      std::cout << HostMatC[INDEX(row, col, C_row)].x << "+" << HostMatC[INDEX(row, col, C_row)].y << "*I "    ;
+    }
+    std::cout << "\n";
+  }
+
+  // on the device
+  cuComplex * DeviceMatA; // d_a - a on the device
+  cuComplex * DeviceMatB; // d_b - b on the device
+  cuComplex * DeviceMatC; // d_c - c on the device
+  cudaStatus = cudaMalloc ((void **)& DeviceMatA , A_row * A_col * sizeof (cuComplex));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for X\n";
+    return EXIT_FAILURE;
+  }
+  
+  // device memory alloc for a
+  cudaStatus = cudaMalloc ((void **)& DeviceMatB , B_row * B_col * sizeof (cuComplex));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for Y\n";
+    return EXIT_FAILURE;
+  }
+  // device memory alloc for b
+  cudaStatus = cudaMalloc ((void **)& DeviceMatC, C_row * C_col * sizeof (cuComplex));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for Z\n";
+    return EXIT_FAILURE;
+  }
+  // device memory alloc for c
+  
+  status = cublasCreate (& handle);  // initialize CUBLAS context
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Failed to initialize handle\n");
+    return EXIT_FAILURE;
+  }
+  
+  // copy matrices from the host to the device
+  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA) , HostMatA, A_row, DeviceMatA, A_row); //a -> d_a
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix A from host to device failed \n");
+    return EXIT_FAILURE;
+  }
+  status = cublasSetMatrix (B_row, B_col, sizeof (*HostMatB) , HostMatB, B_row, DeviceMatB, B_row); //b -> d_b
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix B from host to device failed \n");
+    return EXIT_FAILURE;
+  }
+  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC) , HostMatC, C_row, DeviceMatC, C_row); //c -> d_c
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix C from host to device failed \n");
+    return EXIT_FAILURE;
+  }
+  cuComplex alpha ={alpha_real, alpha_imaginary}; // al =1
+  cuComplex beta ={beta_real, beta_imaginary}; // bet =1
+  // Hermitian matrix - matrix multiplication :
+  // d_c =al*d_a *d_b +bet *d_c ;
+  // d_a - mxm hermitian matrix ; d_b ,d_c - mxn - general matices ;
+  // al ,bet - scalars
+  
+  clk_start = clock();
+  status = cublasChemm(handle, CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER,
+  A_row, B_col, &alpha, DeviceMatA, A_row, DeviceMatB, B_row, &beta, DeviceMatC, C_row);
+  
+  clk_end = clock();
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! kernel execution error\n");
+    return EXIT_FAILURE;
+  }
+  
+  status = cublasGetMatrix (C_row, C_col, sizeof (*HostMatC), DeviceMatC, C_row, HostMatC, C_row); // d_c -> c
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix C from device to host failed\n");
+    return EXIT_FAILURE;
+  }
+ printf ("c after Chemm :\n");
+ for (row = 0; row < C_row; row++) {
+   for (col = 0; col < C_col; col++) { // print c after Chemm
+     std::cout << HostMatC[INDEX(row, col, C_row)].x << "+" << HostMatC[INDEX(row, col, C_row)].y << "*I "    ;
+   }
+   std::cout << "\n";
+ }
+  
+  
+  // printing latency and throughput of the function
+  std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
+        "\nThroughput: " << THROUGHPUT(clk_start, clk_end) << "\n\n";
+  
+
+  cudaStatus = cudaFree (DeviceMatA); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " the device memory deallocation failed for X\n";
+    return EXIT_FAILURE;   
+  }
+  
+  cudaStatus = cudaFree (DeviceMatB); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " the device memory deallocation failed for Y\n";
+    return EXIT_FAILURE;   
+  }
+  
+  cudaStatus = cudaFree (DeviceMatC); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " the device memory deallocation failed for Z\n";
+    return EXIT_FAILURE;   
+  }
+  
+  status  = cublasDestroy (handle); // destroy CUBLAS context
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to uninitialize handle \n");
+    return EXIT_FAILURE;
+  } 
+  
+  delete[] HostMatA; // free host memory
+  delete[] HostMatB; // free host memory
+  delete[] HostMatC; // free host memory
+  return EXIT_SUCCESS ;
+}
