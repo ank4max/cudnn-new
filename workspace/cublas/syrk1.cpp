9936c9c321c3c330749f749754ca685c5aef86f8
ank4max <95414875+ank4max@users.noreply.github.com>
2022-01-03 12:19:27 +0530

    Update syrk1.cpp

--- workspace/cublas/syrk1.cpp
+++ workspace/cublas/syrk1.cpp
@@ -5,60 +5,51 @@
 #include "cublas_v2.h"
 #include <time.h>
 
-#define index(row, column, row_count) (((column)*(row_count))+(row))
-
+#define FIRST_ARG "x_row"    //for comparison with command line argument and initializing value of no. of rows for x
+#define SECOND_ARG "x_col"   //for comparison with command line argument and initializing value of no. of col for x
+#define THIRD_ARG "alpha"   //for comparison with command line argument and initializing value of scalar constant alpha
+#define FOURTH_ARG "beta"     //for comparison with command line argument and initializing value of scalar constant beta
+#define LEN_ARG_FIRST 5      // defining length for   first cmd line argument for comparison
+#define LEN_ARG_SECOND 5     // defining length for  second cmd line argument for comparison
+#define LEN_ARG_THIRD 5      // defining length for  third cmd line argument  for comparison
+#define LEN_ARG_FOURTH 4     // defining length for  fourth cmd line argument for comparison
+#define BEGIN 1              
+#define INDEX(row, col, row_count) (((col)*(row_count))+(row))   // for getting index values matrices
+#define THROUGHPUT(clk_start, clk_end)  ((1e-9 * 2) / (clk_end - clk_start)) 
 
 void PrintMat(float* PrintMatrix, int col, int row) {
   int i, j;
   for (i = 0; i < row; i++) {
     std::cout << "\n";
     for (j = 0; j < col; j++) {
-      std::cout << PrintMatrix[index(i, j, row)] << " ";
+      std::cout << PrintMatrix[INDEX(i, j, row)] << " ";
     }
   }
   std::cout << "\n";
 }
 
-char* SubStr(char* InputArr, int begin, int len) {
-  char* ResultStr = new char[len + 1];
-  for (int i = 0; i < len; i++) {
-    ResultStr[i] = *(InputArr + begin + i);
-  }
-  ResultStr[len] = 0;
-  return ResultStr;
-}
-
-
-
 int main (int argc, char **argv) {
   int x_row, x_col, y_row, y_col;
   float alpha, beta;
-  for (int i = 0; i < argc; i++) {
-    std::cout << argv[i] << std::endl;
+  for (int loop_count = 0; loop_count < argc; loop_count++) {
+    std::cout << argv[loop_count] << std::endl;
   }
   
-  for (int i = 1; i < 7; i++) {
-    int len = sizeof(argv[i]);
-    if (!strcmp(SubStr(argv[i], 1, 5), "x_row"))
-      x_row = atoi(argv[i] + 6);
-    else if (!strcmp(SubStr(argv[i], 1, 5), "x_col"))
-      x_col = atoi(argv[i] + 6);
-    else if (!strcmp(SubStr(argv[i], 1, 5), "y_row"))
-      y_row = atoi(argv[i] + 6);
-    else if (!strcmp(SubStr(argv[i], 1, 5), "y_col"))
-      y_col = atoi(argv[i] + 6);
-    else if (!strcmp(SubStr(argv[i], 1, 5), "alpha"))
-      alpha = atof(argv[i] + 6);
-     else if (!strcmp(SubStr(argv[i], 1, 4), "beta"))
-      beta = atof(argv[i] + 5);
+  for (int loop_count = 1; loop_count < agrc; loop_count++) {
+    int len = sizeof(argv[loop_count]);
+    std::string str(argv[loop_count]);
+    if (!((str.substr(BEGIN, LEN_ARG_FIRST)).compare(FIRST_ARG)))
+      x_row = atoi(argv[loop_count] + 6);
+    else if (!((str.substr(BEGIN, LEN_ARG_SECOND)).compare(SECOND_ARG)))
+      x_col = atoi(argv[loop_count] + LEN_ARG_SECOND + 1);
+    else if (!((str.substr(BEGIN, LEN_ARG_THIRD)).compare(THIRD_ARG)))
+      alpha = atof(argv[loop_count] + LEN_ARG_THIRD + 1);
+    else if (!((str.substr(BEGIN, LEN_ARG_FOURTH)).compare(FOURTH_ARG)))
+      beta = atof(argv[loop_count] + LEN_ARG_FOURTH + 1);
   }
   
-  if (x_row != y_row) {
-    return EXIT_FAILURE;
-  }
-  if (y_row != y_col) {
-    return EXIT_FAILURE;
-  }
+  y_row = x_row;
+  y_col = y_row;
   
   cudaError_t cudaStatus ; 
   cublasStatus_t status ; 
@@ -68,8 +59,8 @@ int main (int argc, char **argv) {
   
   float *HostMatX;                   // nxk matrix a on the host
   float *HostMatY;                   // nxn matrix c on the host
-  HostMatX = (float *) malloc (x_row * x_col * sizeof (float)); // host memory for a
-  HostMatY = (float *) malloc (y_row * y_col * sizeof (float)); // host memory for c
+  HostMatX = new float[x_row * x_col]; // host memory for a
+  HostMatY = new float[y_row * y_col]; // host memory for c
   
   if (HostMatX == 0) {
     fprintf (stderr, "!!!! Host memory allocation error (matrixX)\n");
@@ -86,7 +77,7 @@ int main (int argc, char **argv) {
   for(j = 0; j < y_col; j++) {                                  // 11
     for(i = 0; i < y_row; i++) {                                // 12 ,17
       if(i >= j) {                                           // 13 ,18 ,22
-        HostMatY[index(i, j, y_row)] = (float)ind ++;              // 14 ,19 ,23 ,26
+        HostMatY[INDEX(i, j, y_row)] = (float)ind ++;              // 14 ,19 ,23 ,26
       }                                                     // 15 ,20 ,24 ,27 ,29
     }                                                      // 16 ,21 ,25 ,28 ,30 ,31
   }
@@ -96,7 +87,7 @@ int main (int argc, char **argv) {
   for(i = 0; i < y_row; i++) {
     for(j = 0; j < y_col; j++) {
       if(i >= j) {
-        std::cout << HostMatY[index(i, j, y_row)] << " ";
+        std::cout << HostMatY[INDEX(i, j, y_row)] << " ";
       }
     }
     std::cout << "\n";
@@ -153,12 +144,12 @@ int main (int argc, char **argv) {
   // al ,bet - scalars
   
   
-  start = clock();
+  clk_start = clock();
   
   status = cublasSsyrk(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
   x_row, x_col, &alpha, DeviceMatX, x_row, &beta, DeviceMatY, y_row);
   
-  end = clock();
+  clk_end = clock();
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "!!!! kernel execution error\n");
     return EXIT_FAILURE;
@@ -175,15 +166,15 @@ int main (int argc, char **argv) {
   for(i = 0; i < y_row; i++) {
     for(j = 0; j < y_col; j++) {
       if(i >=j) {  // print the lower triangle
-        std::cout << HostMatY[index(i, j, y_row)] << " " ;  // of c after Ssyrk
+        std::cout << HostMatY[INDEX(i, j, y_row)] << " " ;  // of c after Ssyrk
       }
     }
     printf ("\n");
   }
   
   // printing latency and throughput of the function
-  std::cout << "\nLatency: " <<  ((double)(end - start)) / double(CLOCKS_PER_SEC) <<
-        "\nThroughput: " << (1e-9 * 2) / (end - start) << "\n\n";
+  std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
+        "\nThroughput: " << THROUGHPUT(clk_start, clk_end) << "\n\n";
   
   
   cudaStatus = cudaFree (DeviceMatX); // free device memory
@@ -202,7 +193,32 @@ int main (int argc, char **argv) {
     fprintf (stderr, "!!!! Unable to uninitialize handle \n");
     return EXIT_FAILURE;
   }
-  free (HostMatX); // free host memory
-  free (HostMatY); // free host memory
+  delete[] HostMatX; // free host memory
+  delete[] HostMatY; // free host memory
   return EXIT_SUCCESS ;
 }
+
+// lower triangle of c:
+// 11
+// 12 17
+// 13 18 22
+// 14 19 23 26
+// 15 20 24 27 29
+// 16 21 25 28 30 31
+// a:
+// 11 17 23 29
+// 12 18 24 30
+// 13 19 25 31
+// 14 20 26 32
+// 15 21 27 33
+// 16 22 28 34
+// lower triangle of updated c after Ssyrk : c=al*a*a^T+bet *c
+// 1791
+// 1872 1961
+// 1953 2046 2138
+// 2034 2131 2227 2322
+// 2115 2216 2316 2415 2513
+// 2196 2301 2405 2508 2610 2711
+
+
+
