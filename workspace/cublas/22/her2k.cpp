adf63cd470a5b00bfb8af89c7ede24cebd795d94
ank4max <95414875+ank4max@users.noreply.github.com>
2022-01-06 16:02:30 +0530

    Update her2k.cpp

--- workspace/cublas/22/her2k.cpp
+++ workspace/cublas/22/her2k.cpp
@@ -2,17 +2,12 @@
 #include <string>
 #include "cublas.h"
 #include "cublas_v2.h"
-#include <cuda_runtime.h>
-           
+#include <cuda_runtime.h>          
 #define INDEX(row, col, row_count) (((col) * (row_count)) + (row))    // for getting index values matrices
 #define RANDOM (rand() % 10000 * 1.00) / 100    // to generate random values 
-
+#define THROUGHPUT(clk_start, clk_end)  ((1e-9 * 2) / (clk_end - clk_start)) 
 /* 1e-9 for converting throughput in GFLOP/sec, multiplying by 2 as each multiply-add operation uses two flops and 
  finally dividing it by latency to get required throughput */
-#define THROUGHPUT(clk_start, clk_end)  ((1e-9 * 2) / (clk_end - clk_start)) 
-
-
-
 
 
 void PrintMatrix(cuComplex* Matrix, int matriA_row, int matriA_col) {
@@ -26,7 +21,6 @@ void PrintMatrix(cuComplex* Matrix, int matriA_row, int matriA_col) {
 }
 
 int main (int argc, char **argv) {
-  
   int A_row, A_col, B_row, B_col, C_row, C_col ;
   float alpha_real, alpha_imaginary, beta;
 
@@ -53,29 +47,28 @@ int main (int argc, char **argv) {
     else if (!(cmd_argument.compare("beta")))
       beta = atof(argv[loop_count + 1]);
   }
-
+  
+  //initializing values for matrix B and C        
   B_row = A_row;
   B_col = A_col;
   C_row = A_row;
   C_col = A_row;
   alpha_imaginary = 0.0f;
   
-  cudaError_t cudaStatus; // cudaMalloc status
-  cublasStatus_t status; // CUBLAS functions status
-  cublasHandle_t handle; // CUBLAS context
-  int row, col; // i-row index , j-col. ind.
-  
+  // creating cublas handle
+  cudaError_t cudaStatus; 
+  cublasStatus_t status; 
+  cublasHandle_t handle; 
+  int row, col; 
   time_t clk_start, clk_end;
-  // data preparation on the host
-  cuComplex *HostMatA; // mxm complex matrix a on the host
-  cuComplex *HostMatB; // mxn complex matrix b on the host
-  cuComplex *HostMatC; // mxn complex matrix c on the host
-  HostMatA = new cuComplex[A_row * A_col]; // host memory
-  // alloc for A
-  HostMatB = new cuComplex[B_row * B_col]; // host memory
-  // alloc for B
-  HostMatC = new cuComplex[C_row * C_col]; // host memory
-  // alloc for C
+           
+  // allocating memory for matrices on host
+  cuComplex *HostMatA; 
+  cuComplex *HostMatB; 
+  cuComplex *HostMatC; 
+  HostMatA = new cuComplex[A_row * A_col]; 
+  HostMatB = new cuComplex[B_row * B_col]; 
+  HostMatC = new cuComplex[C_row * C_col]; 
   
   if (HostMatA == 0) {
     fprintf (stderr, "!!!! host memory allocation error (matrix A)\n");
@@ -92,42 +85,41 @@ int main (int argc, char **argv) {
   
   // define the lower triangle of an nxn Hermitian matrix c in
   // lower mode column by column
-  // C:
- 
+  // // setting up values for matrix C
+  // using RANDOM macro to generate random numbers between 0 - 100
   for (col = 0; col < C_col; col++) {                 
     for (row = 0; row < C_row; row++) {                                   
       if(row >= col) {                                        
         HostMatC[INDEX(row, col, C_row)].x = RANDOM;                   
-        HostMatC[INDEX(row, col, C_row)].y = 0.0f;  
-        
+        HostMatC[INDEX(row, col, C_row)].y = 0.0f;     
       }                                                           
     }
-    
   }
   
   // print the lower triangle of C row by row
   std::cout << "lower triangle of C :\n";
-  for (row = 0; row < C_row; row++){
+  for (row = 0; row < C_row; row++) {
     for (col = 0; col < C_col; col++) {
       if(row >= col) {
         std::cout << HostMatC[INDEX(row, col, C_row)].x << "+" << HostMatC[INDEX(row, col, C_row)].y << "*I ";                              
       }
     }
-  std::cout << "\n";
+    std::cout << "\n";
   }
 
-  // define mxn matrices A column by column
-  // A
+  // define  matrix A column by column
+  // setting up values for matrix A
+  // using RANDOM macro to generate random numbers between 0 - 100
   for(col = 0; col < A_col; col++) {           
     for(row = 0; row < A_row; row++) {                      
       HostMatA[INDEX(row, col, A_row)].x = RANDOM;          
-      HostMatA[INDEX(row, col, A_row)].y = 0.0f;
-                   
+      HostMatA[INDEX(row, col, A_row)].y = 0.0f;                
     }
   }
   
-  // define mxn matrices B column by column
-  // B
+  // define  matrix B column by column
+  // setting up values for matrix B
+  // using RANDOM macro to generate random numbers between 0 - 100
   for(col = 0; col < B_col; col++) {           
     for(row = 0; row < B_row; row++) {                      
       HostMatB[INDEX(row, col, B_row)].x = RANDOM;          
@@ -135,7 +127,7 @@ int main (int argc, char **argv) {
     }
   }
  
-  //printing A,b
+  //printing A Matrix
   // print A row by row
   std::cout << "A:\n";
   PrintMatrix(HostMatA, A_row, A_col);
@@ -144,75 +136,78 @@ int main (int argc, char **argv) {
   std::cout << "B:\n";
   PrintMatrix(HostMatB, B_row, B_col);
   
-  // on the device
-  cuComplex * DeviceMatA; // d_a - a on the device
-  cuComplex * DeviceMatB; // d_b - b on the device
-  cuComplex * DeviceMatC; // d_c - c on the device
+  // allocating memory for matrices on device using cudamalloc
+  cuComplex * DeviceMatA; 
+  cuComplex * DeviceMatB; 
+  cuComplex * DeviceMatC; 
   cudaStatus = cudaMalloc ((void **)& DeviceMatA, A_row * A_col * sizeof (cuComplex));
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for A\n";
     return EXIT_FAILURE;
   }
   
-  // device memory alloc for a
   cudaStatus = cudaMalloc ((void **)& DeviceMatB, B_row * B_col * sizeof (cuComplex));
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for B\n";
     return EXIT_FAILURE;
   }
-  // device memory alloc for b
+ 
   cudaStatus = cudaMalloc ((void **)& DeviceMatC, C_row * C_col * sizeof (cuComplex));
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for C\n";
     return EXIT_FAILURE;
   }
-  // device memory alloc for c
   
-  status = cublasCreate (& handle);  // initialize CUBLAS context
+  // initialize CUBLAS context
+  status = cublasCreate (& handle);  
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "!!!! Failed to initialize handle\n");
     return EXIT_FAILURE;
   }
   
-  // copy matrices from the host to the device
-  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA), HostMatA, A_row, DeviceMatA, A_row); //a -> d_a
+  // setting the values of matrices on device
+  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA), HostMatA, A_row, DeviceMatA, A_row); 
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "Copying matrix A from host to device failed \n");
     return EXIT_FAILURE;
   }
-  status = cublasSetMatrix (B_row, B_col, sizeof (*HostMatB) , HostMatB, B_row, DeviceMatB, B_row); //b -> d_b
+  status = cublasSetMatrix (B_row, B_col, sizeof (*HostMatB) , HostMatB, B_row, DeviceMatB, B_row); 
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "Copying matrix B from host to device failed \n");
     return EXIT_FAILURE;
   }
-  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC), HostMatC, C_row, DeviceMatC, C_row); //c -> d_c
+  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC), HostMatC, C_row, DeviceMatC, C_row); 
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "Copying matrix C from host to device failed \n");
     return EXIT_FAILURE;
   }
-  cuComplex alpha ={alpha_real, alpha_imaginary}; // al =1
+           
+  cuComplex alpha ={alpha_real, alpha_imaginary}; 
   // Hermitian rank -2k update :
   // d_c =al*d_a *d_b ^H+\ bar {al }* d_b *a^H + bet *d_c
   // d_c - nxn , hermitian matrix ; d_a ,d_b -nxk general matrices ;
   // al ,bet - scalars
-    
-  clk_start = clock(); 
+  // start variable to store time  
+  clk_start = clock();
+           
   status = cublasCher2k(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
    A_row, A_col, &alpha, DeviceMatA, A_row, DeviceMatB, B_row, &beta, DeviceMatC, C_row);  
   
-  
+  // end variable to store time
   clk_end = clock();
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "!!!! kernel execution error\n");
     return EXIT_FAILURE;
   }
   
-  status = cublasGetMatrix (C_row, C_col, sizeof (*HostMatC), DeviceMatC, C_row, HostMatC, C_row); // d_c -> c
+  // getting the final output
+  status = cublasGetMatrix (C_row, C_col, sizeof (*HostMatC), DeviceMatC, C_row, HostMatC, C_row); 
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "Copying matrix C from device to host failed\n");
     return EXIT_FAILURE;
   }
   
+  // Matrix output
   // print the updated lower triangle of c row by row
   std::cout << "Lower triangle of c after Cher2k :\n";
   for (row = 0; row < C_row; row++) {
@@ -241,21 +236,23 @@ int main (int argc, char **argv) {
     return EXIT_FAILURE;   
   }
   
-  cudaStatus = cudaFree (DeviceMatC); // free device memory
+  cudaStatus = cudaFree (DeviceMatC); 
   if( cudaStatus != cudaSuccess) {
     std::cout << " the device memory deallocation failed for C\n";
     return EXIT_FAILURE;   
   }
   
-  status  = cublasDestroy (handle); // destroy CUBLAS context
+  // destroying cublas handle
+  status  = cublasDestroy (handle); 
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "!!!! Unable to uninitialize handle \n");
     return EXIT_FAILURE;
   } 
   
-  delete[] HostMatA; // free host memory
-  delete[] HostMatB; // free host memory
-  delete[] HostMatC; // free host memory
+  // freeing host memory
+  delete[] HostMatA; 
+  delete[] HostMatB; 
+  delete[] HostMatC; 
   return EXIT_SUCCESS;
 }
   
