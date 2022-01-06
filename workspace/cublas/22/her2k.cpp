4de42924137bbf1510d0570975ce335c1e49cd0e
ank4max <95414875+ank4max@users.noreply.github.com>
2022-01-06 13:53:40 +0530

    Update her2k.cpp

--- workspace/cublas/22/her2k.cpp
+++ workspace/cublas/22/her2k.cpp
@@ -28,7 +28,7 @@ void PrintMatrix(cuComplex* Matrix, int matriA_row, int matriA_col) {
 int main (int argc, char **argv) {
   
   int A_row, A_col, B_row, B_col, C_row, C_col ;
-  float alpha_real, alpha_imaginary;
+  float alpha_real, alpha_imaginary, beta;
 
   std::cout << argv[0] << std::endl;
   for (int loop_count = 1; loop_count < argc; loop_count += 2) {
@@ -49,8 +49,9 @@ int main (int argc, char **argv) {
 
     else if (!(cmd_argument.compare("-alpha_real")))
       alpha_real = atof(argv[loop_count + 1]);
-
-   
+    
+    else if (!(cmd_argument.compare("beta")))
+      beta = atof(argv[loop_count + 1]);
   }
 
   B_row = A_row;
@@ -92,13 +93,13 @@ int main (int argc, char **argv) {
   // define the lower triangle of an nxn Hermitian matrix c in
   // lower mode column by column
   // C:
-  int ind = 11;
+ 
   for (col = 0; col < C_col; col++) {                 
     for (row = 0; row < C_row; row++) {                                   
       if(row >= col) {                                        
-        HostMatC[INDEX(row, col, C_row)].x = ( float )ind;                   
+        HostMatC[INDEX(row, col, C_row)].x = RANDOM;                   
         HostMatC[INDEX(row, col, C_row)].y = 0.0f;  
-        ind++;
+        
       }                                                           
     }
     
@@ -117,30 +118,23 @@ int main (int argc, char **argv) {
 
   // define mxn matrices A column by column
   // A
-  ind =11;
   for(col = 0; col < A_col; col++) {           
     for(row = 0; row < A_row; row++) {                      
-      HostMatA[INDEX(row, col, A_row)].x = ind;          
+      HostMatA[INDEX(row, col, A_row)].x = RANDOM;          
       HostMatA[INDEX(row, col, A_row)].y = 0.0f;
-      ind++;
                    
     }
   }
   
   // define mxn matrices B column by column
-  // A
-  ind =11;
+  // B
   for(col = 0; col < B_col; col++) {           
     for(row = 0; row < B_row; row++) {                      
-      HostMatB[INDEX(row, col, B_row)].x = ind;          
-      HostMatB[INDEX(row, col, B_row)].y = 0.0f;
-      ind++;
-                   
+      HostMatB[INDEX(row, col, B_row)].x = RANDOM;          
+      HostMatB[INDEX(row, col, B_row)].y = 0.0f;                
     }
   }
-  
-  
-  
+ 
   //printing A,b
   // print A row by row
   std::cout << "A:\n";
@@ -154,14 +148,14 @@ int main (int argc, char **argv) {
   cuComplex * DeviceMatA; // d_a - a on the device
   cuComplex * DeviceMatB; // d_b - b on the device
   cuComplex * DeviceMatC; // d_c - c on the device
-  cudaStatus = cudaMalloc ((void **)& DeviceMatA , A_row * A_col * sizeof (cuComplex));
+  cudaStatus = cudaMalloc ((void **)& DeviceMatA, A_row * A_col * sizeof (cuComplex));
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for A\n";
     return EXIT_FAILURE;
   }
   
   // device memory alloc for a
-  cudaStatus = cudaMalloc ((void **)& DeviceMatB , B_row * B_col * sizeof (cuComplex));
+  cudaStatus = cudaMalloc ((void **)& DeviceMatB, B_row * B_col * sizeof (cuComplex));
   if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for B\n";
     return EXIT_FAILURE;
@@ -181,7 +175,7 @@ int main (int argc, char **argv) {
   }
   
   // copy matrices from the host to the device
-  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA) , HostMatA, A_row, DeviceMatA, A_row); //a -> d_a
+  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA), HostMatA, A_row, DeviceMatA, A_row); //a -> d_a
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "Copying matrix A from host to device failed \n");
     return EXIT_FAILURE;
@@ -191,20 +185,19 @@ int main (int argc, char **argv) {
     fprintf (stderr, "Copying matrix B from host to device failed \n");
     return EXIT_FAILURE;
   }
-  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC) , HostMatC, C_row, DeviceMatC, C_row); //c -> d_c
+  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC), HostMatC, C_row, DeviceMatC, C_row); //c -> d_c
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "Copying matrix C from host to device failed \n");
     return EXIT_FAILURE;
   }
   cuComplex alpha ={alpha_real, alpha_imaginary}; // al =1
-  float beta =1.0f;
   // Hermitian rank -2k update :
   // d_c =al*d_a *d_b ^H+\ bar {al }* d_b *a^H + bet *d_c
   // d_c - nxn , hermitian matrix ; d_a ,d_b -nxk general matrices ;
   // al ,bet - scalars
     
   clk_start = clock(); 
-  status = cublasCher2k(handle,CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+  status = cublasCher2k(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
    A_row, A_col, &alpha, DeviceMatA, A_row, DeviceMatB, B_row, &beta, DeviceMatC, C_row);  
   
   
@@ -221,7 +214,7 @@ int main (int argc, char **argv) {
   }
   
   // print the updated lower triangle of c row by row
-  std::cout << " lower triangle of c after Cher2k :\n";
+  std::cout << "Lower triangle of c after Cher2k :\n";
   for (row = 0; row < C_row; row++) {
     for (col = 0; col < C_col; col++) { // print c after Cher2k
       if(row >= col) {
