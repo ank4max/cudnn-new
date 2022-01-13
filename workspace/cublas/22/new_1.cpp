0c0c1f41eed7adfaf0b419e6cd225f041b9e9ef2
ank4max <95414875+ank4max@users.noreply.github.com>
2022-01-13 19:18:03 +0530

    Update new_1.cpp

--- workspace/cublas/22/new_1.cpp
+++ workspace/cublas/22/new_1.cpp
@@ -548,7 +548,183 @@ int Cgemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, floa
   delete[] HostMatC; // free host memory
   return 0;
 }
-           
+ 
+
+int zgemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, float alpha_real, float beta_real) {
+  clk_start = 0;
+  clk_end = 0;
+  float alpha_imaginary = 0.0f;
+  float beta_imaginary = 0.0f; 
+  cuDoubleComplex alpha = {alpha_real,alpha_imaginary};
+  cuDoubleComplex beta = {beta_real, beta_imaginary};
+ 
+  cuDoubleComplex*HostMatA; // mxk matrix A on the host
+  cuDoubleComplex*HostMatB; // kxn matrix B on the host
+  cuDoubleComplex*HostMatC; // mxn matrix C on the host
+  
+  HostMatA = new cuDoubleComplex[A_row * A_col]; // host memory for A
+  HostMatB = new cuDoubleComplex[B_row * B_col]; // host memory for B
+  HostMatC = new cuDoubleComplex[C_row * C_col]; // host memory for C
+  
+  if (HostMatA == 0) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixA)\n");
+    return EXIT_FAILURE;
+  }
+  if (HostMatB == 0) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixB)\n");
+    return EXIT_FAILURE;
+  }
+  if (HostMatC == 0) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixC)\n");
+    return EXIT_FAILURE;
+  }
+
+  int row, col;
+  
+  // define an mxk matrix A column by column
+  // using RANDOM macro to generate random numbers between 0 - 100
+  for (row = 0; row < A_row; row++) {                                              
+    for (col = 0; col < A_col; col++) {                                                   
+      HostMatA[INDEX(row, col, A_row)].x = RANDOM; 
+      HostMatA[INDEX(row, col, A_row)].y = 0.0f;
+    }                                                                                    
+  }                                                                               
+                                                                               
+  // define a kxn matrix B column by column
+  // using RANDOM macro to generate random numbers between 0 - 100
+  for (row = 0; row < B_row; row++) {                                      
+    for (col = 0; col < B_col; col++) {                                                
+      HostMatB[INDEX(row, col, B_row)].x = RANDOM; 
+      HostMatB[INDEX(row, col, B_row)].y = 0.0f;
+    }                                                                         
+  }                                                       
+  
+  // define an mxn matrix C column by column
+  // using RANDOM macro to generate random numbers between 0 - 100
+  for (row = 0; row < C_row; row++) {                             
+    for (col = 0; col < C_col; col++) {                                        
+      HostMatC[INDEX(row, col, C_row)].x = RANDOM;
+      HostMatC[INDEX(row, col, C_row)].y = 0.0f;
+    }                                                                  
+  }
+  
+  // printing input matrices
+  std::cout << "\nMatrix A:\n";
+  PrintCuMatrix(HostMatA, A_row, A_col);
+  std::cout << "\nMatrix B:\n";
+  PrintCuMatrix(HostMatB, B_row, B_col);
+  std::cout << "\nMatrix C:\n";
+  PrintCuMatrix(HostMatC, C_row, C_col);
+
+  // on the device
+  cuDoubleComplex*DeviceMatA; // d_A - A on the device
+  cuDoubleComplex*DeviceMatB; // d_B - B on the device
+  cuDoubleComplex*DeviceMatC; // d_C - C on the device
+
+  cudaStatus = cudaMalloc ((void **) &DeviceMatA , A_row * A_col * sizeof (*HostMatA));
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for A " << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc ((void **) &DeviceMatB , B_row * B_col * sizeof (*HostMatB));
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for B " << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc ((void **) &DeviceMatC , C_row * C_col * sizeof (*HostMatC));
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for C " << std::endl;
+    return EXIT_FAILURE;   
+  }
+  
+  status = cublasCreate (&handle);      // initialize CUBLAS context
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Failed to initialize handle\n");
+    return EXIT_FAILURE;
+  }
+
+  // copy matrices from the host to the device
+  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA), HostMatA, A_row, DeviceMatA, A_row); // A -> d_A
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix A from host to device failed \n");
+    return EXIT_FAILURE;
+  }
+  
+  status = cublasSetMatrix (B_row, B_col, sizeof (*HostMatB), HostMatB, B_row, DeviceMatB, B_row); // B -> d_B
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix B from host to device failed\n");
+    return EXIT_FAILURE;
+  }
+  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC), HostMatC, C_row, DeviceMatC, C_row); // C -> d_C
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix C from host to device failed\n");
+    return EXIT_FAILURE;
+  }
+  
+
+  clk_start = clock();
+
+  // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
+  // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
+  // alpha, beta - scalars
+  status = cublasZgemm (handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
+                        B_col, A_col, &alpha, DeviceMatA, A_row,
+                        DeviceMatB, B_row, &beta, DeviceMatC, C_row);
+  
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! CGemm kernel execution error\n");
+    return EXIT_FAILURE;
+  }
+
+  clk_end = clock();
+  
+  status = cublasGetMatrix (C_row, C_col, sizeof (*HostMatC),
+                            DeviceMatC, C_row, HostMatC, C_row); // copy d_z -> C
+
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to get output matrix C from device\n");
+    return EXIT_FAILURE;
+  }
+  
+  std::cout << "\nMatriz C after zGemm operation is:\n";
+  PrintCuMatrix(HostMatC, C_row, C_col); 
+  
+  // printing latency and throughput of the function
+  std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
+               "\nThroughput: " <<THROUGHPUT(clk_start, clk_end) << "\n\n";
+  
+  cudaStatus = cudaFree (DeviceMatA); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for A" << std::endl;
+    return EXIT_FAILURE;   
+  }
+  
+  cudaStatus = cudaFree (DeviceMatB); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for B" << std::endl;
+    return EXIT_FAILURE;   
+  }
+  
+  cudaStatus = cudaFree (DeviceMatC); // free device memory
+  if( cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for C" << std::endl;
+    return EXIT_FAILURE;   
+  }
+  
+  status  = cublasDestroy (handle); // destroy CUBLAS context
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to uninitialize handle \n");
+    return EXIT_FAILURE;
+  }
+
+  delete[] HostMatA; // free host memory
+  delete[] HostMatB; // free host memory
+  delete[] HostMatC; // free host memory
+  return 0;
+}
+
 
 
 
