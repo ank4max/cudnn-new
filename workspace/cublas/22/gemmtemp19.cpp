8406590a730dc044266ee05954a598cd0d41bea0
ank4max <95414875+ank4max@users.noreply.github.com>
2022-01-19 14:04:32 +0530

    Update gemmtemp19.cpp

--- workspace/cublas/22/gemmtemp19.cpp
+++ workspace/cublas/22/gemmtemp19.cpp
@@ -11,7 +11,7 @@ cublasStatus_t status;
 cublasHandle_t handle;
 clock_t clk_start, clk_end;
 
-template< class Data >
+template<class Data>
 void PrintMatrix(Data* Matrix, int matrix_row, int matrix_col) {
   int row, col;
   for (row = 0; row < matrix_row; row++) {
@@ -23,41 +23,39 @@ void PrintMatrix(Data* Matrix, int matrix_row, int matrix_col) {
   std::cout << "\n";
 }
 
- template< class D >
- void DefineMat(D* Matrix, int matrix_row, int matrix_col) {
-   int row , col;  
-   for (row = 0; row < matrix_row; row++) {                                              
+template<class D>
+void DefineMat(D* Matrix, int matrix_row, int matrix_col) {
+  int row , col;  
+  for (row = 0; row < matrix_row; row++) {                                              
     for (col = 0; col < matrix_col; col++) {                                                   
       Matrix[INDEX(row, col, matrix_row)] = RANDOM;                                      
     }                                                                                    
   }                                                                               
- }
+}
 
- template <class C >
- void DefineCuMat(C* Matrix, int matrix_row, int matrix_col) {
-   int row, col;  
-   for(col = 0; col < matrix_col; col++) {           
+template<class C>
+void DefineCuMat(C* Matrix, int matrix_row, int matrix_col) {
+  int row, col;  
+  for(col = 0; col < matrix_col; col++) {           
     for(row = 0; row < matrix_row; row++) {                      
       Matrix[INDEX(row, col, matrix_row)].x = RANDOM;             
-      Matrix[INDEX(row, col, matrix_row)].y = 0.0f;             
-      
+      Matrix[INDEX(row, col, matrix_row)].y = 0.0f;              
     }
   }
- }
+}
 
-
-template <class P>
+template<class P>
 void PrintCuMatrix(P* Matrix, int matrix_row, int matrix_col) {
   int row, col;
   for (row = 0; row < matrix_row; row++) {
     for (col = 0; col < matrix_col; col++) {
-      std::cout << Matrix[INDEX(row, col, matrix_row)].x << "+" << Matrix[INDEX(row, col, matrix_row)].y << "*I "    ;
+      std::cout << Matrix[INDEX(row, col, matrix_row)].x << "+" << Matrix[INDEX(row, col, matrix_row)].y << "*I ";
     }
     std::cout << "\n";
   } 
 }
 
-template < class T, class T1>
+template<class T, class T1>
 int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 alpha_real, T1 alpha_imaginary, T1 beta_real, T1 beta_imaginary, char n) {
   
   clk_start = 0;
@@ -67,31 +65,32 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
   cuDoubleComplex alpha_z ; 
   cuDoubleComplex beta_z;
 
+  //Host Matrices  Declaration
+  T *HostMatA; 
+  T *HostMatB;
+  T *HostMatC; 
   
-  T *HostMatA; // mxk matrix A on the host
-  T *HostMatB; // kxn matrix B on the host
-  T *HostMatC; // mxn matrix C on the host
-  
-  HostMatA = new T[A_row * A_col]; // host memory for A
-  HostMatB = new T[B_row * B_col]; // host memory for B
-  HostMatC = new T[C_row * C_col]; // host memory for C
+  //host Memory Allocation for Matrices
+  HostMatA = new T[A_row * A_col]; 
+  HostMatB = new T[B_row * B_col]; 
+  HostMatC = new T[C_row * C_col]; 
   
   if (HostMatA == 0) {
     fprintf (stderr, "!!!! Host memory allocation error (matrixA)\n");
     return EXIT_FAILURE;
   }
+  
   if (HostMatB == 0) {
     fprintf (stderr, "!!!! Host memory allocation error (matrixB)\n");
     return EXIT_FAILURE;
   }
+  
   if (HostMatC == 0) {
     fprintf (stderr, "!!!! Host memory allocation error (matrixC)\n");
     return EXIT_FAILURE;
   }
-
-  int row, col;
   
-  // define an mxk matrix A,B,C column by column
+  // define an mxk matrix A,B,C column by column and based on mode passed
   // using RANDOM macro to generate random numbers between 0 - 100
   if(n == 's') {
     DefineMat<float>((float *)HostMatA, A_row, A_col);
@@ -105,11 +104,10 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
     PrintMatrix <float> ((float *)HostMatB, B_row, B_col);
     std::cout << "\nMatrix C:\n";
     PrintMatrix <float> ((float *)HostMatC, C_row, C_col);
-
   }
 
   else if (n == 'd') {
-    DefineMat<double>((double *)HostMatA, A_row, A_col);
+    DefineMat < double>((double *)HostMatA, A_row, A_col);
     DefineMat<double>((double *)HostMatB, B_row, B_col);
     DefineMat<double>((double *)HostMatC, C_row, C_col);
 
@@ -120,7 +118,6 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
     PrintMatrix <double> ((double *)HostMatB, B_row, B_col);
     std::cout << "\nMatix C:\n";
     PrintMatrix <double> ((double *)HostMatC, C_row, C_col);
-
   }
       
   else if(n == 'c') {
@@ -135,6 +132,8 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
     PrintCuMatrix <cuComplex> ((cuComplex *)HostMatB, B_row, B_col);
     std::cout << "\nMatrix C:\n";
     PrintCuMatrix <cuComplex> ((cuComplex *)HostMatC, C_row, C_col);
+    
+    //initializing values for alpha  and beta
     cuComplex alpha ={(float)alpha_real, (float)alpha_imaginary}; 
     cuComplex beta ={(float)beta_real, (float)beta_imaginary};
   }
@@ -168,33 +167,33 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
     PrintMatrix <__half> ((__half *)HostMatB, B_row, B_col);
     std::cout << "\nMatrix C:\n";
     PrintMatrix <__half> ((__half *)HostMatC, C_row, C_col);
-
   }
 
-  // on the device
-  T *DeviceMatA; // d_A - A on the device
-  T *DeviceMatB; // d_B - B on the device
-  T *DeviceMatC; // d_C - C on the device
+  // Memory allocation on the device
+  T *DeviceMatA; 
+  T *DeviceMatB; 
+  T *DeviceMatC; 
 
   cudaStatus = cudaMalloc ((void **) &DeviceMatA , A_row * A_col * sizeof (*HostMatA));
-  if( cudaStatus != cudaSuccess) {
+  if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for A " << std::endl;
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc ((void **) &DeviceMatB , B_row * B_col * sizeof (*HostMatB));
-  if( cudaStatus != cudaSuccess) {
+  if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for B " << std::endl;
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc ((void **) &DeviceMatC , C_row * C_col * sizeof (*HostMatC));
-  if( cudaStatus != cudaSuccess) {
+  if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory allocation failed for C " << std::endl;
     return EXIT_FAILURE;   
   }
   
-  status = cublasCreate (&handle);      // initialize CUBLAS context
+  // initialize CUBLAS context
+  status = cublasCreate (&handle);      
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "!!!! Failed to initialize handle\n");
     return EXIT_FAILURE;
@@ -219,113 +218,106 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
   }
   
   switch (n) {
-    case 's' :
+    case 's':
       std::cout << "Calling  sGemm API\n";
       clk_start = clock();
       // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
       // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
       // alpha, beta - scalars
       status = cublasSgemm (handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
-                          B_col, A_col,(float *) &alpha_real, (float *)DeviceMatA, A_row,
-                          (float *)DeviceMatB, B_row,(float *) &beta_real, (float *)DeviceMatC, C_row);
+                           B_col, A_col, (float *) &alpha_real, (float *) DeviceMatA, A_row,
+                           (float *) DeviceMatB, B_row, (float *) &beta_real, (float *) DeviceMatC, C_row);
     
       if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf (stderr, "!!!!  Sgemm kernel execution error\n");
         return EXIT_FAILURE;
       }
 
-    clk_end = clock();
-    std::cout << "API call ended\n";
-  break;
+      clk_end = clock();
+      std::cout << "API call ended\n";
+      break;
 
-  case 'd':
-    std::cout << "Calling  dGemm API\n";
-    clk_start = clock();
-    // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
-    // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
-    // alpha, beta - scalars
-    status = cublasDgemm (handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
-                        B_col, A_col, (double *)&alpha_real, (double *)DeviceMatA, A_row,
-                        (double *)DeviceMatB, B_row, (double *) &beta_real,(double *) DeviceMatC, C_row);
+    case 'd':
+      std::cout << "Calling  dGemm API\n";
+      clk_start = clock();
+      // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
+      // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
+      // alpha, beta - scalars
+      status = cublasDgemm (handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
+                           B_col, A_col, (double *) &alpha_real, (double *) DeviceMatA, A_row,
+                           (double *) DeviceMatB, B_row, (double *) &beta_real, (double *) DeviceMatC, C_row);
   
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!!  Dgemm kernel execution error\n");
-      return EXIT_FAILURE;
-    
-    }
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Dgemm kernel execution error\n");
+        return EXIT_FAILURE;
+      }
 
-    clk_end = clock();
-    std::cout << "API call ended\n";
-    break;
+      clk_end = clock();
+      std::cout << "API call ended\n";
+      break;
 
     case 'h': 
-    std::cout << "Calling  HGemm API\n";
-    clk_start = clock();
-    // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
-    // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
-    // alpha, beta - scalars
+      std::cout << "Calling  HGemm API\n";
+      clk_start = clock();
+      // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
+      // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
+      // alpha, beta - scalars
 
-    status = cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
-                        B_col, A_col, (__half *)&alpha_real, (__half *)DeviceMatA, A_row,
-                        (__half *)DeviceMatB, B_row, (__half *) &beta_real,(__half *) DeviceMatC, C_row);
+      status = cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
+                          B_col, A_col, (__half *) &alpha_real, (__half *) DeviceMatA, A_row,
+                          (__half *) DeviceMatB, B_row, (__half *) &beta_real, (__half *) DeviceMatC, C_row);
   
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!!  Hgemm kernel execution error\n");
-      return EXIT_FAILURE;
-    
-    }
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Hgemm kernel execution error\n");
+        return EXIT_FAILURE;
+      }
 
-    clk_end = clock();
-    std::cout << "API call ended\n";
-    break;   
+      clk_end = clock();
+      std::cout << "API call ended\n";
+      break;   
     
-    case 'c' :
-    std::cout << "Calling  CGemm API\n";
-    clk_start = clock();
-    // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
-    // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
-    // alpha, beta - scalars
+    case 'c':
+      std::cout << "Calling  CGemm API\n";
+      clk_start = clock();
+      // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
+      // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
+      // alpha, beta - scalars
 
-    status = cublasCgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
-                        B_col, A_col, (cuComplex *)&alpha, (cuComplex *)DeviceMatA, A_row,
-                        (cuComplex *)DeviceMatB, B_row, (cuComplex *) &beta,(cuComplex *) DeviceMatC, C_row);
+      status = cublasCgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
+                          B_col, A_col, (cuComplex *) &alpha, (cuComplex *) DeviceMatA, A_row,
+                         (cuComplex *) DeviceMatB, B_row, (cuComplex *) &beta, (cuComplex *) DeviceMatC, C_row);
   
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!!  Cgemm kernel execution error\n");
-      return EXIT_FAILURE;
-    
-    }
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Cgemm kernel execution error\n");
+        return EXIT_FAILURE;
+      }
 
-    clk_end = clock();
-    std::cout << "API call ended\n";
-    break;
+      clk_end = clock();
+      std::cout << "API call ended\n";
+      break;
     
-    case 'z' :
-    std::cout << "Calling  CGemm API\n";
-    clk_start = clock();
-    // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
-    // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
-    // alpha, beta - scalars
+    case 'z':
+      std::cout << "Calling  ZGemm API\n";
+      clk_start = clock();
+      // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
+      // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
+      // alpha, beta - scalars
 
-    status = cublasZgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
-                        B_col, A_col, (cuDoubleComplex *)&alpha_z, (cuDoubleComplex *)DeviceMatA, A_row,
-                        (cuDoubleComplex *)DeviceMatB, B_row, (cuDoubleComplex *) &beta_z,(cuDoubleComplex *) DeviceMatC, C_row);
+      status = cublasZgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
+                          B_col, A_col, (cuDoubleComplex *) &alpha_z, (cuDoubleComplex *) DeviceMatA, A_row,
+                          (cuDoubleComplex *) DeviceMatB, B_row, (cuDoubleComplex *) &beta_z,(cuDoubleComplex *) DeviceMatC, C_row);
   
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!!  Zgemm kernel execution error\n");
-      return EXIT_FAILURE;
-    
-    }
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Zgemm kernel execution error\n");
+        return EXIT_FAILURE;
+      }
 
-    clk_end = clock();
-    std::cout << "API call ended\n";
-    break;
-
-   
-  
+      clk_end = clock();
+      std::cout << "API call ended\n";
+      break;
   }
   
-  
+  // Copying Matrices from device to host
   status = cublasGetMatrix(C_row, C_col, sizeof (*HostMatC),
                             DeviceMatC, C_row, HostMatC, C_row); // copy d_z -> C
 
@@ -336,38 +328,37 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
   
   std::cout << "\nMatriz C after Gemm operation is:\n";
   if (n == 's') {
-    PrintMatrix <float> ((float *)HostMatC, C_row, C_col); 
+    PrintMatrix<float>((float *) HostMatC, C_row, C_col); 
   }
 
   else if (n =='d') {
-      PrintMatrix <double> ((double *)HostMatC, C_row, C_col); 
+      PrintMatrix <double>((double *) HostMatC, C_row, C_col); 
   }
 
-  else if (n == 'c')
-  {
-      PrintCuMatrix<cuComplex>((cuComplex *)HostMatC, C_row ,C_col);
+  else if (n == 'c') {
+    PrintCuMatrix<cuComplex>((cuComplex *) HostMatC, C_row ,C_col);
   }
 
   else if (n =='z') {
-      PrintCuMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatC, C_row ,C_col);
+    PrintCuMatrix<cuDoubleComplex>((cuDoubleComplex *) HostMatC, C_row ,C_col);
   }
 
   else if (n == 'h') {
-      PrintMatrix <__half> ((__half *)HostMatC, C_row, C_col); 
-      
+    PrintMatrix<__half>((__half *) HostMatC, C_row, C_col); 
   }
+  
   // printing latency and throughput of the function
   std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
                "\nThroughput: " <<THROUGHPUT(clk_start, clk_end) << "\n\n";
   
   cudaStatus = cudaFree (DeviceMatA); // free device memory
-  if( cudaStatus != cudaSuccess) {
+  if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory deallocation failed for A" << std::endl;
     return EXIT_FAILURE;   
   }
   
   cudaStatus = cudaFree (DeviceMatB); // free device memory
-  if( cudaStatus != cudaSuccess) {
+  if(cudaStatus != cudaSuccess) {
     std::cout << " The device memory deallocation failed for B" << std::endl;
     return EXIT_FAILURE;   
   }
@@ -391,11 +382,6 @@ int Gemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T1 al
   
 }
   
-
-
-
-
-
 int main (int argc, char **argv) {
   
   int A_row, A_col, B_row, B_col, C_row, C_col;
@@ -446,7 +432,7 @@ int main (int argc, char **argv) {
   
   if (n =='s') {
     std::cout << "Calling sGemm function\n";
-    Gemm <float,float> (A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
+    Gemm<float, float>(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
   }
   else if(n == 'd') {
     alpha_real =  (double)alpha_real;
@@ -454,26 +440,22 @@ int main (int argc, char **argv) {
     beta_real = (double)beta_real;
     beta_imaginary = (double) beta_imaginary;
     std::cout << "Calling DGemm function\n";
-    Gemm <double,double> (A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
-
+    Gemm<double, double>(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
   }
 
   else if(n == 'c') {
-  
     std::cout << "Calling CGemm function\n";
-    Gemm <cuComplex,float> (A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
-
+    Gemm<cuComplex, float>(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
   }
 
   else if(n == 'z') {
-  
     std::cout << "Calling ZGemm function\n";
-    Gemm <cuDoubleComplex, double> (A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
-
+    Gemm<cuDoubleComplex, double>(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
   }
+           
   else if(n == 'h') {
-      Gemm <__half, __half> (A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);
-      
+    std::cout << "Calling Hgemm function\n";
+    Gemm<__half, __half>(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real, beta_imaginary, n);  
   }
 
   return EXIT_SUCCESS;
