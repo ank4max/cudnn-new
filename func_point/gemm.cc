34010b630dc8d3f17bfc326d872e0301227721ff
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-11 12:38:03 +0530

    Update gemm.cc

--- func_point/gemm.cc
+++ func_point/gemm.cc
@@ -1,3 +1,4 @@
+#include <unordered_map>
 #include "cublas_gemm_test.h"
 
 template<class T>
@@ -364,11 +365,74 @@ int Gemm<T>::GemmApiCall() {
   return EXIT_SUCCESS;
 }
 
+void mode_S(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary
+            double beta_real, double beta_imaginary) {
+            
+  float alpha = (float)alpha_real;
+  float beta = (float)beta_real;
+
+  Gemm<float> Sgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'S' );
+  Sgemm.GemmApiCall();
+}
+
+void mode_D(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary
+            double beta_real, double beta_imaginary) {
+            
+  double alpha = alpha_real;
+  double beta = beta_real;
+
+  Gemm<double> Dgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'D');
+  Dgemm.GemmApiCall();
+}
+
+void mode_C(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary
+            double beta_real, double beta_imaginary) {
+            
+  cuComplex alpha = {(float)alpha_real, (float)alpha_imaginary};
+  cuComplex beta = {(float)beta_real, (float)beta_imaginary};
+
+  Gemm<cuComplex> Cgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'C');
+  Cgemm.GemmApiCall();
+
+}
+
+void mode_Z(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary
+            double beta_real, double beta_imaginary) {
+            
+  cuDoubleComplex alpha = {alpha_real, alpha_imaginary};
+  cuDoubleComplex beta = {beta_real, beta_imaginary};
+
+  Gemm<cuDoubleComplex> Zgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'Z');
+  Zgemm.GemmApiCall();
+
+}
+
+void mode_H(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary
+            double beta_real, double beta_imaginary) {
+
+  __half alpha = (__half)alpha_real;
+  __half beta = (__half)beta_real;
+
+  Gemm<cuDoubleComplex> Hgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'H');
+  Hgemm.GemmApiCall();
+}
+
+void (*cublas_func_ptr[])(int, int, int, int, int, int, double, double, double, double) = {
+  mode_S, mode_D, mode_C, mode_Z, mode_H
+};
+
 int main(int argc, char **argv) {
 
   int A_row, A_col, B_row, B_col, C_row, C_col, status;
   double alpha_real, alpha_imaginary, beta_real, beta_imaginary;
   char mode;
+    
+  std::unordered_map<char, int> mode_index;
+  mode_index['S'] = 0;
+  mode_index['D'] = 1;
+  mode_index['C'] = 2;
+  mode_index['Z'] = 3;
+  mode_index['H'] = 4;
 
   std::cout << "\n\n" << argv[0] << std::endl;
   for (int loop_count = 1; loop_count < argc; loop_count += 2) {
@@ -412,53 +476,9 @@ int main(int argc, char **argv) {
   C_row = A_row;
   C_col = B_col;
 
-  //! Calling Gemm API based on mode
-  switch (mode) {
-    case 'S': {
-      float alpha = (float)alpha_real;
-      float beta = (float)beta_real;
-
-      Gemm<float> Sgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, mode);
-      status = Sgemm.GemmApiCall();
-      break;
-    }
-
-    case 'D': {
-      double alpha = alpha_real;
-      double beta = beta_real;
-
-      Gemm<double> Dgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, mode);
-      status = Dgemm.GemmApiCall();
-      break;
-    }
-
-    case 'C': {
-      cuComplex alpha = {(float)alpha_real, (float)alpha_imaginary};
-      cuComplex beta = {(float)beta_real, (float)beta_imaginary};
-
-      Gemm<cuComplex> Cgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, mode);
-      status = Cgemm.GemmApiCall();
-      break;
-    }
-
-    case 'Z': {
-      cuDoubleComplex alpha = {alpha_real, alpha_imaginary};
-      cuDoubleComplex beta = {beta_real, beta_imaginary};
-
-      Gemm<cuDoubleComplex> Zgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, mode);
-      status = Zgemm.GemmApiCall();
-      break;
-    }
-
-    case 'H': {
-      __half alpha = (__half)alpha_real;
-      __half beta = (__half)beta_real;
-
-      Gemm<__half> Hgemm(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, mode);
-      status = Hgemm.GemmApiCall();
-      break;
-    }
-  }
+  (*cublas_func_ptr[mode_index[mode]])(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real
+                                       , beta_imaginary);
+  
 
   return EXIT_SUCCESS;
 }
