7ce61765e97025db7e9f4e93767e0bf8828a5fe0
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-11 16:47:54 +0530

    Create syr2k.cc

---
+++ func_point/syr2k.cc
@@ -0,0 +1,429 @@
+#include <unordered_map>
+#include "cublas_syr2k_test.h"
+
+
+template<class T>
+Syr2k<T>::Syr2k(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T alpha, T beta, char mode)
+    : A_row(A_row), A_col(A_col), B_row(B_row), B_col(B_col),
+      C_row(C_row), C_col(C_col), alpha(alpha), beta(beta), mode(mode) {}
+
+template<class T>
+void Syr2k<T>::FreeMemory() {
+  //! Free Host Memory
+  if (HostMatrixA)
+    delete[] HostMatrixA;
+
+  if (HostMatrixB)
+    delete[] HostMatrixB;
+
+  if (HostMatrixC)
+    delete[] HostMatrixC;
+
+  //! Free Device Memory
+  cudaStatus = cudaFree(DeviceMatrixA);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for A" << std::endl;
+  }
+
+  cudaStatus = cudaFree(DeviceMatrixB);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for B" << std::endl;
+  }
+
+  cudaStatus = cudaFree(DeviceMatrixC);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for C" << std::endl;
+  }
+
+  //! Destroy CuBLAS context
+  status  = cublasDestroy(handle);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to uninitialize handle \n");
+  }
+}
+
+template<class T>
+int Syr2k<T>::Syr2kApiCall() {
+  //! Allocating Host Memory for Matrices
+  HostMatrixA = new T[A_row * A_col];
+  HostMatrixB = new T[B_row * B_col];
+  HostMatrixC = new T[C_row * C_col];
+
+  if (!HostMatrixA) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixA)\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  if (!HostMatrixB) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixB)\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  if (!HostMatrixC) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixC)\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  /**
+   * Switch Case - To Initialize and Print input matrices based on mode passed,
+   *  A and B are general Matrices,
+   *  C is a symmetric Matrix
+   */
+  switch (mode) {
+    case 'S': {
+      util::InitializeMatrix<float>((float *)HostMatrixA, A_row, A_col);
+      util::InitializeMatrix<float>((float *)HostMatrixB, B_row, B_col);
+      util::InitializeSymmetricMatrix<float>((float *)HostMatrixC, C_row, C_col);
+
+      std::cout << "\nMatrix C:\n";
+      util::PrintSymmetricMatrix<float>((float *)HostMatrixC, C_row, C_col);
+      std::cout << "\nMatrix A:\n";
+      util::PrintMatrix<float>((float *)HostMatrixA, A_row, A_col);
+      std::cout << "\nMatrix B:\n";
+      util::PrintMatrix<float>((float *)HostMatrixB, B_row, B_col);
+          
+      break;
+    }
+
+    case 'D': {
+      util::InitializeMatrix<double>((double *)HostMatrixA, A_row, A_col);
+      util::InitializeMatrix<double>((double *)HostMatrixB, B_row, B_col);
+      util::InitializeSymmetricMatrix<double>((double *)HostMatrixC, C_row, C_col);
+
+      std::cout << "\nMatrix C:\n";
+      util::PrintSymmetricMatrix<double>((double *)HostMatrixC, C_row, C_col);
+      std::cout << "\nMatrix A:\n";
+      util::PrintMatrix<double>((double *)HostMatrixA, A_row, A_col);
+      std::cout << "\nMatrix B:\n";
+      util::PrintMatrix<double>((double *)HostMatrixB, B_row, B_col); 
+      break;  
+    }
+
+    case 'C': {
+      util::InitializeComplexMatrix<cuComplex>((cuComplex *)HostMatrixA, A_row, A_col);
+      util::InitializeComplexMatrix<cuComplex>((cuComplex *)HostMatrixB, B_row, B_col);
+      util::InitializeSymmetricComplexMatrix<cuComplex>((cuComplex *)HostMatrixC, C_row, C_col);
+
+      std::cout << "\nMatrix C:\n";
+      util::PrintSymmetricComplexMatrix<cuComplex>((cuComplex *)HostMatrixC, C_row, C_col);
+      std::cout << "\nMatrix A:\n";
+      util::PrintComplexMatrix<cuComplex>((cuComplex *)HostMatrixA, A_row, A_col);
+      std::cout << "\nMatrix B:\n";
+      util::PrintComplexMatrix<cuComplex>((cuComplex *)HostMatrixB, B_row, B_col);
+      break; 
+    }
+                            
+    case 'Z': {
+      util::InitializeComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixA, A_row, A_col);
+      util::InitializeComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixB, B_row, B_col);
+      util::InitializeSymmetricComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixC, C_row, C_col);
+
+      std::cout << "\nMatrix C:\n";
+      util::PrintSymmetricComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixC, C_row, C_col);
+      std::cout << "\nMatrix A:\n";
+      util::PrintComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixA, A_row, A_col);
+      std::cout << "\nMatrix B:\n";
+      util::PrintComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixB, B_row, B_col);
+      break; 
+    }
+  }
+
+  //! Allocating Device Memory for Matrices using cudaMalloc()
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixA, A_row * A_col * sizeof(*HostMatrixA));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for A " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixB, B_row * B_col * sizeof(*HostMatrixB));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for B " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixC, C_row * C_col * sizeof(*HostMatrixC));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for C " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  //! Initializing CUBLAS context
+  status = cublasCreate(&handle);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Failed to initialize handle\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  //! Copying values of Host matrices to Device matrices using cublasSetMatrix()
+
+  status = cublasSetMatrix(A_row, A_col, sizeof(*HostMatrixA), HostMatrixA, A_row, DeviceMatrixA, A_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix A from host to device failed\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  status = cublasSetMatrix(B_row, B_col, sizeof(*HostMatrixB), HostMatrixB, B_row, DeviceMatrixB, B_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix B from host to device failed\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  status = cublasSetMatrix(C_row, C_col, sizeof(*HostMatrixC), HostMatrixC, C_row, DeviceMatrixC, C_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix C from host to device failed\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  /**
+   * API call to performs symmetric rank- 2k update : C = alpha * (A * B^T + B * A^T) + beta * C
+   */
+  
+  /**
+   * The possible error values returned by this API and their meanings are listed below :
+   * CUBLAS_STATUS_SUCCESS - The operation completed successfully
+   * CUBLAS_STATUS_NOT_INITIALIZED - The library was not initialized
+   * CUBLAS_STATUS_INVALID_VALUE - The parameters n, k <0
+   * CUBLAS_STATUS_EXECUTION_FAILED - The function failed to launch on the GPU
+   */
+  
+  switch (mode) {
+    case 'S': {
+      std::cout << "\nCalling Ssyr2k API\n";
+      clk_start = clock();
+
+      status = cublasSsyr2k(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+                            A_row, A_col, (float *)&alpha, 
+                            (float *)DeviceMatrixA, A_row, (float *)DeviceMatrixB, 
+                            B_row, (float *)&beta, (float *)DeviceMatrixC, C_row);
+        
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Ssyr2k kernel execution error\n");
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "Ssyr2k API call ended\n";
+      break;
+    }
+                            
+    case 'D': {
+      std::cout << "\nCalling Dsyr2k API\n";
+      clk_start = clock();
+
+      status = cublasDsyr2k(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+                            A_row, A_col, (double *)&alpha,
+                            (double *)DeviceMatrixA, A_row, (double *)DeviceMatrixB, 
+                            B_row, (double *)&beta, (double *)DeviceMatrixC, C_row);
+        
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Dsyr2k kernel execution error\n");
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "Dsyr2k API call ended\n";
+      break;
+    }
+
+    case 'C': {
+      std::cout << "\nCalling Csyr2k API\n";
+      clk_start = clock();
+
+      status = cublasCsyr2k(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+                            A_row, A_col, (cuComplex *)&alpha,
+                            (cuComplex *)DeviceMatrixA, A_row, 
+                            (cuComplex *)DeviceMatrixB, B_row,
+                            (cuComplex *)&beta, (cuComplex *)DeviceMatrixC, C_row);
+        
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Csyr2k kernel execution error\n");
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "Csyr2k API call ended\n";
+      break;
+    }
+      
+    case 'Z': {
+      std::cout << "\nCalling Zsyr2k API\n";
+      clk_start = clock();
+
+      status = cublasZsyr2k(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+                            A_row, A_col, (cuDoubleComplex *)&alpha,
+                            (cuDoubleComplex *)DeviceMatrixA, A_row,
+                            (cuDoubleComplex *)DeviceMatrixB, B_row,
+                            (cuDoubleComplex *)&beta,
+                            (cuDoubleComplex *)DeviceMatrixC, C_row);
+        
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Zsyr2k kernel execution error\n");
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "Zsyr2k API call ended\n";
+      break;
+    }
+
+  }
+
+  //! Copy Matrix C, holding resultant matrix, from Device to Host using cublasGetMatrix()
+
+  status = cublasGetMatrix(C_row, C_col, sizeof(*HostMatrixC),
+                           DeviceMatrixC, C_row, HostMatrixC, C_row);
+
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to get output matrix C from device\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "\nMatrix C after " << mode << "syr2k operation is:\n";
+
+  //! Print the final resultant Matrix C
+  switch (mode) {
+    case 'S': {
+      util::PrintSymmetricMatrix<float>((float *)HostMatrixC, C_row, C_col); 
+      break;
+    }
+
+    case 'D': {
+      util::PrintSymmetricMatrix<double>((double *)HostMatrixC, C_row, C_col);  
+      break;
+    }
+
+    case 'C': {
+      util::PrintSymmetricComplexMatrix<cuComplex>((cuComplex *)HostMatrixC, C_row ,C_col); 
+      break;
+    }
+
+    case 'Z': {
+      util::PrintSymmetricComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixC, C_row ,C_col); 
+      break;
+    }
+
+  }
+
+  long long total_operations = A_row * A_col * B_col;
+
+  //! Print Latency and Throughput of the API
+  std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / (double)(CLOCKS_PER_SEC) <<
+               "\nThroughput: " << THROUGHPUT(clk_start, clk_end, total_operations) << "\n\n";
+
+  FreeMemory();
+
+  return EXIT_SUCCESS;
+}
+
+void mode_S(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+  float alpha = (float)alpha_real;
+  float beta = (float)beta_real;
+
+  Syr2k<float> Ssyr2k(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'S');
+  Ssyr2k.Syr2kApiCall();
+}
+
+void mode_D(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+  double alpha = alpha_real;
+  double beta = beta_real;
+
+  Syr2k<double> Dsyr2k(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'D');
+  Dsyr2k.Syr2kApiCall();
+}
+
+void mode_C(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+  cuComplex alpha = {(float)alpha_real, (float)alpha_imaginary};
+  cuComplex beta = {(float)beta_real, (float)beta_imaginary};
+
+  Syr2k<cuComplex> Csyr2k(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'C');
+  Csyr2k.Syr2kApiCall();
+}
+
+void mode_Z(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+  cuDoubleComplex alpha = {alpha_real, alpha_imaginary};
+  cuDoubleComplex beta = {beta_real, beta_imaginary};
+
+  Syr2k<cuDoubleComplex> Zsyr2k(A_row, A_col,B_row, B_col, C_row, C_col, alpha, beta, 'Z');
+  Zsyr2k.Syr2kApiCall();
+
+}
+
+
+void (*cublas_func_ptr[])(int, int, int, int, int, int, double, double, double, double) = {
+  mode_S, mode_D, mode_C, mode_Z
+};
+
+
+int main(int argc, char **argv) {
+  int A_row, A_col, B_row, B_col, C_row, C_col, status;
+  double alpha_real, alpha_imaginary, beta_real, beta_imaginary;
+  char mode;
+  
+  std::unordered_map<char, int> mode_index;
+  mode_index['S'] = 0;
+  mode_index['D'] = 1;
+  mode_index['C'] = 2;
+  mode_index['Z'] = 3;
+
+  std::cout << "\n\n" << argv[0] << std::endl;
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::cout << argv[loop_count] << " ";
+    if (loop_count + 1 < argc)
+      std::cout << argv[loop_count + 1] << std::endl;
+  }
+  std::cout << std::endl;
+
+  // Reading cmd line arguments and initializing the parameters
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::string cmd_argument(argv[loop_count]);
+    if (!(cmd_argument.compare("-A_row")))
+      A_row = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-A_column")))
+      A_col = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-alpha_real")))
+      alpha_real = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-alpha_imaginary")))
+      alpha_imaginary = std::stod(argv[loop_count + 1]);
+    
+    else if (!(cmd_argument.compare("-beta_real")))
+      beta_real = std::stod(argv[loop_count + 1]);
+    
+    else if (!(cmd_argument.compare("-beta_imaginary")))
+      beta_imaginary = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-mode")))
+      mode = *(argv[loop_count + 1]);
+  }
+
+  B_row = A_row;
+  B_col = A_col;
+  C_row = A_row;
+  C_col = A_row;
+
+  (*cublas_func_ptr[mode_index[mode]])(A_row, A_col, B_row, B_col, C_row, C_col, alpha_real, alpha_imaginary, beta_real
+                                       , beta_imaginary);
+  
+
+  return EXIT_SUCCESS;
+}
