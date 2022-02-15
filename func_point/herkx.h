a091daf1d322a55d64f8c12709ac1123c193c48a
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-15 15:17:50 +0530

    Create herkx.h

---
+++ func_point/herkx.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#include "cublas_utility.h"
+
+/*
+ * 1e-9 for converting throughput in GFLOP/sec, multiplying by 2 as each multiply-add operation uses two flops and
+ * finally dividing it by latency to get required throughput
+ */
+#define THROUGHPUT(clk_start, clk_end, operations) ((1e-9 * 2 * operations) / (clk_end - clk_start))
+
+/**
+ * Class Herkx contains Herkx API which performs variation of the Hermitian rank- k update : C = alpha * A * B^H + beta * C
+ * \param A - n x k general matrix,
+ * \param B - n x k general matrix
+ * \param C - n x n Hermitian matrix stored in lower or upper mode
+ * \param alpha - scalar
+ * \param beta - scalar
+ */
+template<class T>
+class Herkx {
+  public:
+    /**
+     * Herkx constructor - To initialize the class varibles using initializer list,
+     * sets up the API mode, alpha, beta and dimension of matrices
+     */
+    Herkx(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T alpha, double beta, char mode);
+
+    /**
+     * FreeMemory function - To free the allocated memory when program is ended or in case of any error
+     */
+    void FreeMemory();
+
+    /**
+     * HerkxAPICall function - To allocate Host and Device memory,
+          sets up matrices and calls Herkx API based on the mode passed
+     */
+    int HerkxApiCall();
+
+  private:
+    int A_row, A_col, B_row, B_col, C_row, C_col;
+    char mode;
+    T *HostMatrixA;
+    T *HostMatrixB;
+    T *HostMatrixC;
+    T *DeviceMatrixA;
+    T *DeviceMatrixB;
+    T *DeviceMatrixC;
+    T alpha;
+    double beta;
+    cudaError_t cudaStatus;
+    cublasStatus_t status;
+    cublasHandle_t handle;
+    clock_t clk_start, clk_end;
+};
