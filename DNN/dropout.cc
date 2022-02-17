5f493e88435680c24d49814c04cd6405222e70ad
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-17 18:02:36 +0530

    Update dropout.cc

--- DNN/dropout.cc
+++ DNN/dropout.cc
@@ -1,28 +1,16 @@
 %%writefile max1.cc
 
 #include "dropout.h"
-
-//print function
-
-void printArr3D(float * arr, int arrH, int arrW, int batchSize) {
-  for(int i = 0; i < batchSize; i++) {
-    for(int j = 0; j < arrH; j++) {
-      for(int k = 0; k < arrW; k++) {
-        printf("%f ", arr[i*arrH*arrW + j*arrW + k]);
-      } 
-      printf("\n");
-    }
-    printf("\n");
-  }
-}
+#include "cudnn_utility.h
 
 Dropout::Dropout(int batch, int channel, int height, int width, float drop_rate)
     : batch(batch), channel(channel), height(height), width(width), drop_rate(drop_rate) {}
 
 int Dropout::FreeMemory() {
   
-  if(h_arr) {
-    delete[] h_arr;
+  if(HostOutput) {
+    delete[] HostOutput;
+    HostOutput = nullptr;
   }
   
   cudaStatus = cudaFree(states);
@@ -68,21 +56,14 @@ int Dropout::DropoutForwardApiCall() {
   int size = batch * channel * height * width;
   int size_bytes = size * sizeof(float);  
 
-  std::cout<<"batch is " <<batch<<std::endl;
-  std::cout<<"channel is " <<channel<<std::endl;
-  std::cout<<"height is " <<height<<std::endl;
-  std::cout<<"width is " <<width<<std::endl;
-  std::cout<<"droprate is " <<drop_rate<<std::endl;
-
   float InputData[size];
-  for (int i = 0; i < size; i++) {
-    InputData[i] = rand() % 255;
-  }
+  Util::InitializeInputTensor(InputData, size);
   
-  // Create cudnn context
+  //! Create cudnn context
   status = cudnnCreate(&handle_);
   if( status != CUDNN_STATUS_SUCCESS) {
     printf(" Unable to initialize handle\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
 	
@@ -91,84 +72,98 @@ int Dropout::DropoutForwardApiCall() {
   status = cudnnCreateDropoutDescriptor(&dropout_descriptor);
   if( status != CUDNN_STATUS_SUCCESS) {
     printf("\nCreating dropout descriptor failed\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
   status = cudnnCreateTensorDescriptor(&dropout_in_out_descriptor);
   if( status != CUDNN_STATUS_SUCCESS) {
     printf("\nCreating dropout_in_out descriptor failed\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }  
   
   status = cudnnSetTensor4dDescriptor(dropout_in_out_descriptor, format, dtype, batch, channel, height, width);
   if( status != CUDNN_STATUS_SUCCESS) {
     printf("\nSetting dropout_in_out descriptor failed\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
-  
+ 
+  //! Getting states size
   status = cudnnDropoutGetStatesSize(handle_, (size_t *)&dropout_state_size);
   if( status != CUDNN_STATUS_SUCCESS) {
-    printf("\nGet dropout state size error\n");
+    printf("\nGet dropout state size error\n"); 
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
+  //! Getting reverse space size
   status = cudnnDropoutGetReserveSpaceSize(dropout_in_out_descriptor, (size_t *)&dropout_reserve_size);
   if( status != CUDNN_STATUS_SUCCESS) {
     printf("\nGet dropout Reverse size error\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
-  // Allocate memory for states and reserve space
+  //! Allocate memory for states and reserve space
   cudaStatus = cudaMalloc(&states, dropout_state_size);
   if( cudaStatus != cudaSuccess) {
     printf("\nDevice Memory allocation error\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
   cudaStatus = cudaMalloc(&dropout_reserve_space, dropout_reserve_size);
   if( cudaStatus != cudaSuccess) {
     printf("\nDevice Memory allocation error\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
 
   status = cudnnSetDropoutDescriptor(dropout_descriptor, handle_, drop_rate, states, dropout_state_size, /*Seed*/time(NULL));
   if( status != CUDNN_STATUS_SUCCESS) {
     printf("\nSetting dropout descriptor failed\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
-  cudaStatus = cudaMalloc(&d_dropout_out, size_bytes);
+  cudaStatus = cudaMalloc(&DeviceOutput, size_bytes);
   if( cudaStatus != cudaSuccess) {
     printf("\nDevice Memory allocation error \n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
- 
-  cudaStatus = cudaMalloc(&d_input,size_bytes);
+  cudaStatus = cudaMalloc(&DeviceInput, size_bytes);
   if( cudaStatus != cudaSuccess) {
     printf("\nDevice Memory allocation error \n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
-  cudaStatus = cudaMemcpy(d_input, InputData, size_bytes, cudaMemcpyHostToDevice);
+  //! Copying data from host to device
+  cudaStatus = cudaMemcpy(DeviceInput, InputData, size_bytes, cudaMemcpyHostToDevice);
   if( cudaStatus != cudaSuccess) {
     printf("\nFailed to copy data from host to device \n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
   std::cout << "\nInput \n";
-  printArr3D(InputData, height, width, batch * channel);
+  util::DropoutPrint(InputData, height, width, batch * channel);
   
   //! API call
   clk_start=clock();
       
-  status = cudnnDropoutForward(handle_, dropout_descriptor, dropout_in_out_descriptor, d_input, dropout_in_out_descriptor,
-			       d_dropout_out, dropout_reserve_space, dropout_reserve_size);
+  status = cudnnDropoutForward(handle_, dropout_descriptor, dropout_in_out_descriptor, DeviceInput, dropout_in_out_descriptor,
+			       DeviceOutput, dropout_reserve_space, dropout_reserve_size);
   
   clk_stop=clock();
       
   if( status != CUDNN_STATUS_SUCCESS) {
     printf(" API faied to execute\n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
 	
@@ -177,17 +172,17 @@ int Dropout::DropoutForwardApiCall() {
                "\nLatency: " << ((double)(clk_stop - clk_start))/CLOCKS_PER_SEC <<
                "\nThroughput: " << THROUGHPUT(clk_start, clk_stop, size) << std::endl;
   
-  std::cout << "\nDropout \n";
-
-
-  h_arr = new float[size];
-  cudaStatus = cudaMemcpy(h_arr, d_dropout_out, size_bytes, cudaMemcpyDeviceToHost);
+  //! Copying values from device to host
+  HostOutput = new float[size];
+  cudaStatus = cudaMemcpy(HostOutput, DeviceOutput, size_bytes, cudaMemcpyDeviceToHost);
   if( cudaStatus != cudaSuccess) {
     printf("\nFailed to copy data from Device to host \n");
+    FreeMemory();
     return EXIT_FAILURE;   
   }
   
-  printArr3D(h_arr, height, width, batch * channel);
+  std::cout << "\nDropout \n";
+  DropoutPrint(HostOutput, height, width, batch * channel);
   FreeMemory();
   return EXIT_SUCCESS;
 }
