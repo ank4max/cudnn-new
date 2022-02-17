df3134b30209638fafafc8fc1beb695eb22b084c
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-17 12:50:31 +0530

    Create activation.h

---
+++ DNN/activation.h
@@ -0,0 +1,53 @@
+%%writefile activation.h
+#include <iostream>
+#include <string>
+#include <cuda_runtime.h>
+#include <cudnn.h>
+#include <time.h>
+#include <iomanip>
+
+/*
+ * 1e-9 for converting throughput in GFLOP/sec, multiplying by 2 as each multiply-add operation uses two flops and
+ * finally dividing it by latency to get required throughput
+ */
+#define THROUGHPUT(clk_start, clk_end, operations) ((1e-9 * 2 * operations) / (clk_end - clk_start))
+
+/**
+ * Class Activation contains Activation API which performs activation operation on Input Image based on mode :
+ */
+class Activation {
+  public:
+    /**
+     * Activation constructor - To initialize the class varibles using initializer list
+     */
+    Activation(int batch, int channel, int height, int width, char* activation_mode);
+
+    /**
+     * FreeMemory function - To free the allocated memory when program is ended or in case of any error
+     */
+    void FreeMemory();
+
+    /**
+     * ActivationApiCall which performs activation operation on input image
+     */
+    int ActivationApiCall();
+
+  private:
+    int batch, channel, height, width;
+    std::string activation_mode;
+    float *InputDeviceArray;
+    float *OutputDeviceArray;
+    float *output;
+    clock_t clk_start, clk_stop;
+    cudaError_t cudaStatus;
+    cudnnStatus_t status;
+    cudnnHandle_t handle_;
+    cudnnDataType_t dtype = CUDNN_DATA_FLOAT;
+    cudnnTensorFormat_t format = CUDNN_TENSOR_NCHW;
+    cudnnActivationDescriptor_t activation;
+    cudnnActivationMode_t mode;
+    cudnnNanPropagation_t prop;
+    cudnnTensorDescriptor_t x_desc;
+    cudnnTensorDescriptor_t y_desc;
+
+};
