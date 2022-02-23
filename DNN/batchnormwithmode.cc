dc5c22f1f98c6e70a2ebe854de45ca6aa983f475
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-23 12:36:35 +0530

    Update batchnormwithmode.cc

--- DNN/batchnormwithmode.cc
+++ DNN/batchnormwithmode.cc
@@ -1,14 +1,12 @@
-%%writefile max.cc
+%%writefile max1.cc
 #include "batchnorm.h"
 #include "cudnn_utility.h"
 
-#define ONE 1.0
-#define ZERO 0.0
 #define ExponentialAverageFactor 0.5
 #define EPSILON 0.001
 
-BatchNormalizationForward::BatchNormalizationForward(int batch, int channel, int height, int width, char* batchnorm_mode)
-    : batch(batch), channel(channel), height(height), width(width), batchnorm_mode(batchnorm_mode) {}
+BatchNormalizationForward::BatchNormalizationForward(int batch, int channel, int height, int width, char *mode)
+    : batch(batch), channel(channel), height(height), width(width), mode(mode) {}
 
 void BatchNormalizationForward::FreeMemory() {
   if (HostInputTensor) {
@@ -43,62 +41,62 @@ void BatchNormalizationForward::FreeMemory() {
 
   cudaStatus = cudaFree(DeviceInputTensor);
   if (cudaStatus != cudaSuccess) {
-    printf("Device input memory deallocation error\n");
+    std::cout << "Device input memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(DeviceOutputTensor);
   if (cudaStatus != cudaSuccess) {
-    printf("Device output memory deallocation error\n");
+    std::cout << "Device output memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(device_scale);
   if( cudaStatus != cudaSuccess) {
-    printf(" Device scale memory deallocation error\n");
+    std::cout << " Device scale memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(device_offset);
   if( cudaStatus != cudaSuccess) {
-    printf(" Device offset memory deallocation error\n");
+    std::cout << " Device offset memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(device_running_mean);
   if( cudaStatus != cudaSuccess) {
-    printf(" Device running_mean memory deallocation error\n");
+    std::cout << " Device running_mean memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(device_running_var);
   if( cudaStatus != cudaSuccess) {
-    printf(" Device running_var memory deallocation error\n");
+    std::cout << " Device running_var memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(device_saved_mean);
   if( cudaStatus != cudaSuccess) {
-    printf(" Device saved_mean memory deallocation error\n");
+    std::cout << " Device saved_mean memory deallocation error\n" << std::endl;
   }
 
   cudaStatus = cudaFree(device_saved_inv_var);
   if( cudaStatus != cudaSuccess) {
-    printf(" Device saved_inv_var memory deallocation error\n");
+    std::cout << " Device saved_inv_var memory deallocation error\n" << std::endl;
   }
 
   status = cudnnDestroyTensorDescriptor(input_desc);
   if (status != CUDNN_STATUS_SUCCESS) {
-    printf(" Unable to Destroy input Descriptor\n");
+    std::cout << " Unable to Destroy input Descriptor\n" << std::endl;
   }
 
   status = cudnnDestroyTensorDescriptor(output_desc);
   if (status != CUDNN_STATUS_SUCCESS) {
-    printf(" Unable to Destroy output Descriptor\n");
+    std::cout << " Unable to Destroy output Descriptor\n" << std::endl;
   }
 
   status = cudnnDestroyTensorDescriptor(mean_descriptor);
   if (status != CUDNN_STATUS_SUCCESS) {
-    printf(" Unable to Destroy mean Descriptor\n");
+    std::cout << " Unable to Destroy mean Descriptor\n" << std::endl;
   }
 
   status = cudnnDestroy(handle_);
   if (status != CUDNN_STATUS_SUCCESS) {
-    printf("Unable to uninitialize handle\n");
+    std::cout << "Unable to uninitialize handle\n" << std::endl;
   }
 }
 
@@ -120,7 +118,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   // Create cudnn handle
   status = cudnnCreate(&handle_);
   if( status != CUDNN_STATUS_SUCCESS) {
-    printf(" Unable to initialize handle\n");
+    std::cout << " Unable to initialize handle\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -128,7 +126,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
 
   status = cudnnCreateTensorDescriptor(&input_desc);
   if(status != CUDNN_STATUS_SUCCESS) {
-    printf(" Creating input tensor descriptor error\n");
+    std::cout << " Creating input tensor descriptor error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -136,14 +134,14 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   status = cudnnSetTensor4dDescriptor(input_desc, data_format, data_type,
                                       batch, channel, height, width);
   if( status != CUDNN_STATUS_SUCCESS) {
-    printf(" Setting input tensor descriptor error\n");
+    std::cout << " Setting input tensor descriptor error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   status = cudnnCreateTensorDescriptor(&output_desc);
   if(status != CUDNN_STATUS_SUCCESS) {
-    printf(" Creating output tensor descriptor error\n");
+    std::cout << " Creating output tensor descriptor error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -151,20 +149,20 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   status = cudnnSetTensor4dDescriptor(output_desc, data_format, data_type,
                                       batch, channel, height, width);
   if( status != CUDNN_STATUS_SUCCESS) {
-    printf(" Setting output tensor descriptor error\n");
+    std::cout << " Setting output tensor descriptor error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc(&DeviceInputTensor, size_bytes);
   if(cudaStatus != cudaSuccess) {
-    printf(" Memory allocation on device for input tensor failed\n");
+    std::cout << " Memory allocation on device for input tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
   cudaStatus = cudaMalloc(&DeviceOutputTensor, size_bytes);
   if(cudaStatus != cudaSuccess) {
-    printf(" Memory allocation on device for output tensor failed\n");
+    std::cout << " Memory allocation on device for output tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -173,7 +171,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   cudaStatus = cudaMemcpy(DeviceInputTensor, HostInputTensor, size_bytes,
                           cudaMemcpyHostToDevice);
   if (cudaStatus != cudaSuccess) {
-    fprintf (stderr, "!!!! Setting up values on device for Input tensor failed\n");
+    std::cout << "!!!! Setting up values on device for Input tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -191,33 +189,35 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
    *    This mode is similar to CUDNN_BATCHNORM_SPATIAL but it
    *    can be faster for some tasks.
    */
-  
-  if (batchnorm_mode == "batchnorm_per_activation") {
-      bn_mode = CUDNN_BATCHNORM_PER_ACTIVATION;
+  if (mode == "batchnorm_per_activation") {
+    bn_mode = CUDNN_BATCHNORM_PER_ACTIVATION;
+    std::cout << "\nUsing batchnorm mode : CUDNN_BATCHNORM_PER_ACTIVATION\n";
   }
 
-  else if (batchnorm_mode == "batchnorm_spatial") {
-      bn_mode = CUDNN_BATCHNORM_SPATIAL;
+  else if (mode == "batchnorm_spatial") {
+    bn_mode = CUDNN_BATCHNORM_SPATIAL;
+    std::cout << "\nUsing batchnorm mode : CUDNN_BATCHNORM_SPATIAL\n";
   }
   
-  else if (batchnorm_mode =="batchnorm_spatial_persistent") {
-     bn_mode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+  else {
+    bn_mode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+    std::cout <<"\nUsing batchnorm mode : CUDNN_BATCHNORM_SPATIAL_PERSISTENT\n";
   }
 
-  float alpha[channel] = {ONE};
-  float beta[channel] = {ZERO};
+  alpha = ALPHA_INITIAL_VALUE;
+  beta= BETA_INITIAL_VALUE;
 
   status = cudnnCreateTensorDescriptor(&mean_descriptor);
   if(status != CUDNN_STATUS_SUCCESS) {
-    printf(" Creating mean descriptor error\n");
+    std::cout << " Creating mean descriptor error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   status = cudnnSetTensor4dDescriptor(mean_descriptor, data_format, data_type,
-                                      ONE, mean_size, ONE, ONE);
+                                      1, mean_size, 1, 1);
   if( status != CUDNN_STATUS_SUCCESS) {
-    printf(" Setting mean descriptor error\n");
+    std::cout << " Setting mean descriptor error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -229,36 +229,36 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
 
   //! initialize scale, offset, running_mean, running_var
   for (int index = 0; index < mean_size; index++) {
-    scale[index] = ONE;
-    offset[index] = ONE;
-    running_mean[index] = ONE;
-    running_var[index] = ONE;
+    scale[index] = INITIAL_VALUE;
+    offset[index] = INITIAL_VALUE;
+    running_mean[index] = INITIAL_VALUE;
+    running_var[index] = INITIAL_VALUE;
   }
 
   cudaStatus = cudaMalloc(&device_scale, mean_size_bytes);
   if( cudaStatus != cudaSuccess) {
-    printf(" the device memory allocation failed for scale\n");
+    std::cout << " the device memory allocation failed for scale\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc(&device_offset, mean_size_bytes);
   if( cudaStatus != cudaSuccess) {
-    printf(" the device memory allocation failed for offset\n");
+    std::cout << " the device memory allocation failed for offset\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc(&device_running_mean, mean_size_bytes);
   if( cudaStatus != cudaSuccess) {
-    printf(" the device memory allocation failed for running_mean\n");
+    std::cout << " the device memory allocation failed for running_mean\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc(&device_running_var, mean_size_bytes);
   if( cudaStatus != cudaSuccess) {
-    printf(" the device memory allocation failed for running_var\n");
+    std::cout << " the device memory allocation failed for running_var\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -266,7 +266,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   cudaStatus = cudaMemcpy(device_scale, scale, mean_size_bytes,
                           cudaMemcpyHostToDevice);
   if (cudaStatus != cudaSuccess) {
-    fprintf (stderr, "!!!! Setting up values on device for scale tensor failed\n");
+    std::cout << "!!!! Setting up values on device for scale tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -274,7 +274,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   cudaStatus = cudaMemcpy(device_offset, offset, mean_size_bytes,
                           cudaMemcpyHostToDevice);
   if (cudaStatus != cudaSuccess) {
-    fprintf (stderr, "!!!! Setting up values on device for scale tensor failed\n");
+    std::cout << "!!!! Setting up values on device for scale tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -282,7 +282,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   cudaStatus = cudaMemcpy(device_running_mean, running_mean, mean_size_bytes,
                           cudaMemcpyHostToDevice);
   if (cudaStatus != cudaSuccess) {
-    fprintf (stderr, "!!!! Setting up values on device for scale tensor failed\n");
+    std::cout << "!!!! Setting up values on device for scale tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -290,49 +290,37 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   cudaStatus = cudaMemcpy(device_running_var, running_var, mean_size_bytes,
                           cudaMemcpyHostToDevice);
   if (cudaStatus != cudaSuccess) {
-    fprintf (stderr, "!!!! Setting up values on device for scale tensor failed\n");
+    std::cout << "!!!! Setting up values on device for scale tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc(&device_saved_mean, mean_size_bytes);
   if( cudaStatus != cudaSuccess) {
-    printf(" the device memory allocation failed\n");
+    std::cout << " the device memory allocation failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   cudaStatus = cudaMalloc(&device_saved_inv_var, mean_size_bytes);
   if( cudaStatus != cudaSuccess) {
-    printf(" the device memory allocation failed\n");
+    std::cout << " the device memory allocation failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
 
   clk_start=clock();
-  status = cudnnBatchNormalizationForwardTraining(
-                /*handle=*/handle_,
-                /*mode=*/bn_mode,
-                /**alpha=*/&alpha,
-                /**beta=*/&beta,
-                /*xDesc=*/input_desc,
-                /**x=*/DeviceInputTensor,
-                /*yDesc=*/output_desc,
-                /**y=*/DeviceOutputTensor,
-                /*bnScaleBiasMeanVarDesc=*/mean_descriptor,
-                /*bnScaleData=*/device_scale,
-                /*bnBiasData=*/device_offset,
-                /*exponentialAverageFactor=*/ExponentialAverageFactor,
-                /*resultRunningMeanData=*/device_running_mean,
-                /*resultRunningVarianceData=*/device_running_var,
-                /*epsilon=*/EPSILON,
-                /*resultSaveMean=*/device_saved_mean,
-                /*resultSaveInvVariance=*/device_saved_inv_var);
+  status = cudnnBatchNormalizationForwardTraining(handle_, bn_mode,
+                &alpha, &beta, input_desc, DeviceInputTensor, output_desc,
+                DeviceOutputTensor, mean_descriptor, device_scale,
+                device_offset, ExponentialAverageFactor, device_running_mean,
+                device_running_var,EPSILON, device_saved_mean,
+                device_saved_inv_var);
 
   clk_stop=clock();
 
   if( status != CUDNN_STATUS_SUCCESS) {
-    printf(" Kernel execution error\n");
+    std::cout << " Kernel execution error\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -341,7 +329,7 @@ int BatchNormalizationForward::BatchNormalizationForwardApiCall() {
   cudaStatus = cudaMemcpy(HostOutputTensor, DeviceOutputTensor, size_bytes,
                           cudaMemcpyDeviceToHost);
   if (cudaStatus != cudaSuccess) {
-    fprintf (stderr, "!!!! Setting up values on host for output tensor failed\n");
+    std::cout << "!!!! Setting up values on host for output tensor failed\n" << std::endl;
     FreeMemory();
     return EXIT_FAILURE;
   }
@@ -370,7 +358,7 @@ int main(int argc, char** argv) {
   std::cout << std::endl;
 
   int batch, channel, height, width;
-  char *batchnorm_mode;
+  char *mode;
 
   //! Reading cmd line arguments and initializing the required parameters
   for (int loop_count = 1; loop_count < argc; loop_count += 2) {
@@ -386,11 +374,12 @@ int main(int argc, char** argv) {
 
     else if (!(cmd_argument.compare("-width")))
       width = std::atoi(argv[loop_count + 1]);
-    
+
     else if (!(cmd_argument.compare("-batchnorm_mode")))
-      batchnorm_mode = (argv[loop_count + 1]);
+      mode = (argv[loop_count + 1]);
   }
 
-  BatchNormalizationForward batchnormalizationforward(batch, channel, height, width, batchnorm_mode);
-  batchnormalizationforward.BatchNormalizationForwardApiCall();
+  BatchNormalizationForward batchnormalization(batch, channel, height, width, mode);
+  batchnormalization.BatchNormalizationForwardApiCall();
 }
+
