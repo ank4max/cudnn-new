1e990fec170e95a679c268807162248774123522
ank4max <95414875+ank4max@users.noreply.github.com>
2022-02-24 17:13:49 +0530

    Update conv.h

--- DNN/conv.h
+++ DNN/conv.h
@@ -23,7 +23,7 @@ class ConvolutionForward {
      */
     ConvolutionForward(int batch, int channel, int height, int width, int filter_batch,
                        int filter_channel, int filter_height, int filter_width, int padding, 
-                       int stride, int dilation, char *mode, char *fwd_preference);
+                       int stride, int dilation, char *mode, char *preference);
 
     /**
      * FreeMemory function - To free the allocated memory when program is ended or in case of any error
@@ -48,7 +48,7 @@ class ConvolutionForward {
     int dilation_width;
     float alpha;
     float beta;
-    std::string mode, fwd_preference;
+    std::string mode, preference;
     float *workspace_data;
     float *HostInputTensor;
     float *HostOutputTensor;
