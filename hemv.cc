173dce1c13449c34112157a104f62de3c76cd6cc
ank4max <95414875+ank4max@users.noreply.github.com>
2022-03-08 09:32:51 +0530

    Create hemv.cc

---
+++ hemv.cc
@@ -0,0 +1,309 @@
+%%writefile test.cc
+#include <unordered_map>
+#include "hemv.h"
+
+template<class T>
+Hemv<T>::Hemv(int A_row, int A_col, int vector_length, T alpha, T beta, char mode) : A_row(A_row), A_col(A_col), 
+    vector_length(vector_length), alpha(alpha), beta(beta), mode(mode) {}
+
+template<class T>
+void Hemv<T>::FreeMemory() {
+  //! Free Host Memory
+  if (HostMatrixA)
+    delete[] HostMatrixA;
+
+  if (HostVectorX)
+    delete[] HostVectorX;
+
+  if (HostVectorY)
+    delete[] HostVectorY;
+
+  //! Free Device Memory
+  cudaStatus = cudaFree(DeviceMatrixA);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for A" << std::endl;
+  }
+
+  cudaStatus = cudaFree(DeviceVectorX);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for B" << std::endl;
+  }
+
+  cudaStatus = cudaFree(DeviceVectorY);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for C" << std::endl;
+  }
+
+  //! Destroy CuBLAS context
+  status  = cublasDestroy(handle);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to uninitialize handle \n");
+  }
+}
+
+template<class T>
+int Hemv<T>::HemvApiCall() {
+  //! Allocating Host Memory for Matrix and Vectors
+  HostMatrixA = new T[A_row * A_col];
+  HostVectorX = new T[vector_length];
+  HostVectorY = new T[vector_length];
+
+  if (!HostMatrixA) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixA)\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  if (!HostVectorX) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixB)\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  if (!HostVectorY) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixC)\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  /**
+   * Switch Case - To Initialize and Print input matrix and vectors based on mode passed,
+   * A is Hermitian matrix, 
+   * X and Y are vectors
+   */
+  switch (mode) {
+
+    case 'C': {
+      util::InitializeSymmetricComplexMatrix<cuComplex>((cuComplex *)HostMatrixA, A_row, A_col);
+      util::InitializeComplexVector<cuComplex>((cuComplex *)HostVectorX, vector_length);
+      util::InitializeComplexVector<cuComplex>((cuComplex *)HostVectorY, vector_length);
+
+      std::cout << "\nMatrix A:\n";
+      util::PrintSymmetricComplexMatrix<cuComplex>((cuComplex *)HostMatrixA, A_row, A_col);
+      std::cout << "\nVector X:\n";
+      util::PrintComplexVector<cuComplex>((cuComplex *)HostVectorX, vector_length);
+      std::cout << "\nVector Y:\n";
+      util::PrintComplexVector<cuComplex>((cuComplex *)HostVectorY, vector_length);
+      
+      break;
+    }
+    case 'Z': {
+      util::InitializeSymmetricComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixA, A_row, A_col);
+      util::InitializeComplexVector<cuDoubleComplex>((cuDoubleComplex *)HostVectorX, vector_length);
+      util::InitializeComplexVector<cuDoubleComplex>((cuDoubleComplex *)HostVectorY, vector_length);
+
+      std::cout << "\nMatrix A:\n";
+      util::PrintSymmetricComplexMatrix<cuDoubleComplex>((cuDoubleComplex *)HostMatrixA, A_row, A_col);
+      std::cout << "\nVector X:\n";
+      util::PrintComplexVector<cuDoubleComplex>((cuDoubleComplex *)HostVectorX, vector_length);
+      std::cout << "\nVector Y:\n";
+      util::PrintComplexVector<cuDoubleComplex>((cuDoubleComplex *)HostVectorY, vector_length);      
+      
+      break;
+    }
+  }
+  
+  //! Allocating Device Memory for Matrix and Vectors using cudaMalloc()
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixA, A_row * A_col * sizeof(*HostMatrixA));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for A " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc((void **)&DeviceVectorX, vector_length * sizeof(*HostVectorX));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for X " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc((void **)&DeviceVectorY, vector_length * sizeof(*HostVectorY));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for Y " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  //! Initializing CUBLAS context
+  status = cublasCreate(&handle);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Failed to initialize handle\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  
+  //! Copying values of Host matrices to Device matrices using cublasSetMatrix()
+
+  status = cublasSetMatrix(A_row, A_col, sizeof(*HostMatrixA), HostMatrixA, A_row, DeviceMatrixA, A_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying matrix A from host to device failed\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  status = cublasSetVector(vector_length, sizeof(*HostVectorX), HostVectorX, 
+                           VECTOR_LEADING_DIMENSION, DeviceVectorX, VECTOR_LEADING_DIMENSION);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying vector X from host to device failed\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  status = cublasSetVector(vector_length, sizeof(*HostVectorY), HostVectorY, 
+                           VECTOR_LEADING_DIMENSION, DeviceVectorY, VECTOR_LEADING_DIMENSION);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "Copying vector Y from host to device failed\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  
+  
+  
+  switch (mode) {
+
+    case 'C': {
+      std::cout << "\nCalling Chemv API\n";
+      clk_start = clock();
+
+      status = cublasChemv(handle, CUBLAS_FILL_MODE_LOWER, vector_length, (cuComplex *)&alpha, (cuComplex *)DeviceMatrixA, A_row, 
+                           (cuComplex *)DeviceVectorX, VECTOR_LEADING_DIMENSION, (cuComplex *)&beta, (cuComplex *)DeviceVectorY, VECTOR_LEADING_DIMENSION);
+
+
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Chemv kernel execution error\n");
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "Chemv API call ended\n";
+      break;
+    }
+
+    case 'Z': {
+      std::cout << "\nCalling Zhemv API\n";
+      clk_start = clock();
+
+      status = cublasZhemv(handle, CUBLAS_FILL_MODE_LOWER, vector_length, (cuDoubleComplex *)&alpha, (cuDoubleComplex *)DeviceMatrixA, A_row, 
+                           (cuDoubleComplex *)DeviceVectorX, VECTOR_LEADING_DIMENSION, (cuDoubleComplex *)&beta, (cuDoubleComplex *)DeviceVectorY, VECTOR_LEADING_DIMENSION);
+
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf (stderr, "!!!!  Zhemv kernel execution error\n");
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "Zhemv API call ended\n";
+      break;
+    }
+  }
+  
+  //! Copy Vector Y, holding resultant vector, from Device to Host using cublasGetVector()
+  status = cublasGetVector(vector_length, sizeof (*HostVectorY), DeviceVectorY, 1, HostVectorY, 1);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf (stderr, "!!!! Unable to get output matrix C from device\n");
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "\nVector Y after " << mode << "hemv operation is:\n";
+  switch (mode) {
+
+    case 'C': {
+      util::PrintComplexVector<cuComplex>((cuComplex *)HostVectorY, vector_length);
+      break;
+    }
+
+    case 'Z': {
+      util::PrintComplexVector<cuDoubleComplex>((cuDoubleComplex *)HostVectorY, vector_length);
+      break;
+    }
+  }
+
+  long long total_operations = A_row * vector_length;
+
+  //! printing latency and throughput of the function
+  std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
+               "\nThroughput: " << THROUGHPUT(clk_start, clk_end, total_operations) << "\n\n";
+
+  FreeMemory();
+
+  return EXIT_SUCCESS;
+}
+
+
+void mode_C(int A_row, int A_col, int vector_length, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+            
+  cuComplex alpha = {(float)alpha_real, (float)alpha_imaginary};
+  cuComplex beta = {(float)beta_real, (float)beta_imaginary};
+
+  Hemv<cuComplex> Chemv(A_row, A_col, vector_length, alpha, beta, 'C');
+  Chemv.HemvApiCall(); 
+}
+
+void mode_Z(int A_row, int A_col, int vector_length, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+            
+  cuDoubleComplex alpha = {alpha_real, alpha_imaginary};
+  cuDoubleComplex beta = {beta_real, beta_imaginary};
+
+  Hemv<cuDoubleComplex> Zhemv(A_row, A_col, vector_length, alpha, beta, 'Z');
+  Zhemv.HemvApiCall(); 
+}
+
+void (*cublas_func_ptr[])(int, int, int, double, double, double, double) = {
+ mode_C, mode_Z
+};
+
+int main(int argc, char **argv) {
+  int A_row, A_col, vector_length;
+  double alpha_real, alpha_imaginary, beta_real, beta_imaginary;
+  char mode;
+
+  std::unordered_map<char, int> mode_index;
+  mode_index['C'] = 0;
+  mode_index['Z'] = 1;
+
+  std::cout << "\n\n" << argv[0] << std::endl;
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::cout << argv[loop_count] << " ";
+    if (loop_count + 1 < argc)
+      std::cout << argv[loop_count + 1] << std::endl;
+  }
+  std::cout << std::endl;
+
+  //! reading cmd line arguments and initializing the required parameters
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::string cmd_argument(argv[loop_count]);
+
+    if (!(cmd_argument.compare("-A_row")))
+      A_row = atoi(argv[loop_count + 1]);
+
+   else if (!(cmd_argument.compare("-alpha_real")))
+      alpha_real = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-alpha_imaginary")))
+      alpha_imaginary = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-beta_real")))
+      beta_real = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-beta_imaginary")))
+      beta_imaginary = std::stod(argv[loop_count + 1]);   
+
+    else if (!(cmd_argument.compare("-mode")))
+      mode = *(argv[loop_count + 1]);
+  }
+
+   //! initializing values for matrix B and C
+   A_col = A_row;
+   vector_length = A_row;
+
+
+  (*cublas_func_ptr[mode_index[mode]])(A_row, A_col, vector_length, alpha_real, alpha_imaginary, 
+                                       beta_real, beta_imaginary);
+
+  return 0;
+}
