bce823ff784495ef13152ed677550e46ab16ddfd
ank4max <95414875+ank4max@users.noreply.github.com>
2022-03-14 16:24:24 +0530

    Create hbmv.h

---
+++ hbmv.h
@@ -0,0 +1,59 @@
+%%writefile hbmv.h
+#include <iostream>
+#include <string>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#include "cublas_utility.h"
+
+/*
+ * 1e-9 for converting throughput in GFLOP/sec, multiplying by 2 as each multiply-add operation uses two flops and
+ * finally dividing it by latency to get required throughput
+ */
+#define THROUGHPUT(clk_start, clk_end, operations) ((1e-9 * 2 * operations) / (clk_end - clk_start))
+
+/**
+ * Class Hbmv contains Hbmv API which performs Hermitian banded matrix-vector multiplication : Y = alpha * A * X + beta * Y    
+ * \param A - n x n hermitian banded matrix,
+ * \param X - vector of lenght n
+ * \param Y -  vector of length n
+ * \param alpha - scalar
+ * \param beta - scalar
+ */
+template<class T>
+class Hbmv {
+  public:
+    /**
+     * Hbmv constructor - To initialize the class varibles using initializer list,
+     * sets up the API mode, alpha, beta and dimension of matrix and vectors
+     */
+    Hbmv(int A_row, int A_col, int vector_length, int sub_diagonals, T alpha, T beta, char mode);
+
+    /**
+     * FreeMemory function - To free the allocated memory when program is ended or in case of any error
+     */
+    void FreeMemory();
+
+    /**
+     * HbmvAPICall function - To allocate Host and Device memory,
+          sets up matrix and vectors and calls Hbmv API based on the mode passed
+     */
+    int HbmvApiCall();
+
+  private:
+    int A_row, A_col;
+    int vector_length;
+    int sub_diagonals;
+    char mode;
+    T *HostMatrixA;
+    T *HostVectorX;
+    T *HostVectorY;
+    T *DeviceMatrixA;
+    T *DeviceVectorX;
+    T *DeviceVectorY;
+    T alpha;
+    T beta;
+    cudaError_t cudaStatus;
+    cublasStatus_t status;
+    cublasHandle_t handle;
+    clock_t clk_start, clk_end;
+};
