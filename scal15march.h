42b3a293aba479afb15c91614bd8c73e7f1a4606
ank4max <95414875+ank4max@users.noreply.github.com>
2022-03-15 13:20:15 +0530

    Create scal15march.h

---
+++ scal15march.h
@@ -0,0 +1,49 @@
+%%writefile scal.h
+#include <iostream>
+#include <string>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#include "cublas_utility.h"
+
+/*
+ * 1e-9 for converting throughput in GFLOP/sec, multiplying by 2 as each multiply-add operation uses two flops and
+ * finally dividing it by latency to get required throughput
+ */
+#define THROUGHPUT(clk_start, clk_end, operations) ((1e-9 * 2 * operations) / (clk_end - clk_start))
+
+/**
+ * Class Scal contains Scal API which scales the vector x by the scalar α and overwrites it with the result : X = alpha * X
+ * \param X - vector of length n
+ * \param alpha - scalar
+ */
+template<class T, class T1>
+class Scal {
+  public:
+    /**
+     * Scal constructor - To initialize the class varibles using initializer list,
+     * sets up the API mode, alpha and dimension of vectors
+     */
+    Scal(int vector_length, T1 alpha, char mode);
+
+    /**
+     * FreeMemory function - To free the allocated memory when program is ended or in case of any error
+     */
+    void FreeMemory();
+
+    /**
+     * ScalAPICall function - To allocate Host and Device memory,
+          sets up vector and calls Scal API based on the mode passed
+     */
+    int ScalApiCall();
+
+  private:
+    int vector_length;
+    char mode;
+    T *HostVectorX;
+    T *DeviceVectorX;
+    T1 alpha;
+    cudaError_t cudaStatus;
+    cublasStatus_t status;
+    cublasHandle_t handle;
+    clock_t clk_start, clk_end;
+};
