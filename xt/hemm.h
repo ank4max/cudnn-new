ebc2b30fb8c0098ac172b3c9aa81c26c7c848d07
ank4max <95414875+ank4max@users.noreply.github.com>
2022-03-17 15:59:37 +0530

    Create hemm.h

---
+++ xt/hemm.h
@@ -0,0 +1,56 @@
+%%writefile hemm.h
+#include <iostream>
+#include <string>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#include "cublasXt.h"
+#include "cublas_utility.h"
+
+/* 
+ * 1e-9 for converting throughput in GFLOP/sec, multiplying by 2 as each multiply-add operation uses two flops and 
+ * finally dividing it by latency to get required throughput 
+ */
+#define THROUGHPUT(clk_start, clk_end, operations) ((1e-9 * 2 * operations) / (clk_end - clk_start)) 
+
+/**
+ * Class Hemm contains Hemm API which performs Hermitian matrix-matrix multiplication : 
+ * \f$ C = alpha * A * B + beta * C \f$
+ * \param A - m x m Hermitian matrix,
+ * \param B - m x n general matrix
+ * \param C - m x n general matrix
+ * \param alpha - scalar
+ * \param beta - scalar
+ */
+template<class T>
+class Hemm {
+  public:
+    /**
+     * Hemm constructor - To initialize the class varibles using initializer list,
+     * sets up the API mode, alpha, beta and dimension of matrices
+     */
+    Hemm(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T alpha, T beta, char mode);
+
+    /**
+     * FreeMemory function - To free the allocated memory when program is ended or in case of any error
+     */
+    void FreeMemory();
+    
+    /**
+     * HemmAPICall function - To allocate Host and Device memory,
+     * sets up matrices and calls Hemm API based on the mode passed
+     */
+    int HemmApiCall();
+
+  private:
+    int A_row, A_col, B_row, B_col, C_row, C_col;
+    char mode;
+    T *HostMatrixA;
+    T *HostMatrixB;
+    T *HostMatrixC;
+    T alpha;
+    T beta;
+    cudaError_t cudaStatus; 
+    cublasStatus_t status; 
+    cublasXtHandle_t handle;
+    clock_t clk_start, clk_end;
+};
