912c5fac4c2a3dd091d064d9888fc738390cef3d
ank4max <95414875+ank4max@users.noreply.github.com>
2022-04-14 16:00:16 +0530

    Create gemmEx.cc

---
+++ gemmEx.cc
@@ -0,0 +1,345 @@
+%%writefile gemmEx.cc
+#include <unordered_map>
+#include "gemmEx.h"
+
+template<class T>
+gemmEx<T>::gemmEx(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, T alpha, T beta, char mode)
+    : A_row(A_row), A_col(A_col), B_row(B_row), B_col(B_col),
+      C_row(C_row), C_col(C_col), alpha(alpha), beta(beta), mode(mode) {}
+
+template<class T>
+void gemmEx<T>::FreeMemory() {
+  //! Free Host Memory
+  if (HostMatrixA)
+    delete[] HostMatrixA;
+
+  if (HostMatrixB)
+    delete[] HostMatrixB;
+
+  if (HostMatrixC)
+    delete[] HostMatrixC;
+
+  //! Free Device Memory
+  cudaStatus = cudaFree(DeviceMatrixA);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for A" << std::endl;
+  }
+
+  cudaStatus = cudaFree(DeviceMatrixB);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for B" << std::endl;
+  }
+
+  cudaStatus = cudaFree(DeviceMatrixC);
+  if (cudaStatus != cudaSuccess) {
+    std::cout << " The device memory deallocation failed for C" << std::endl;
+  }
+
+  //! Destroy CuBLAS context
+  status  = cublasDestroy(handle);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "!!!! Unable to uninitialize handle \n";
+  }
+}
+
+template<class T>
+int gemmEx<T>::gemmExApiCall() {
+  //! Allocating Host Memory for Matrices
+  HostMatrixA = new T[A_row * A_col];
+  HostMatrixB = new T[B_row * B_col];
+  HostMatrixC = new T[C_row * C_col];
+
+  if (!HostMatrixA) {
+    std::cout << "!!!! Host memory allocation error (matrixA)\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  if (!HostMatrixB) {
+    std::cout << "!!!! Host memory allocation error (matrixB)\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  if (!HostMatrixC) {
+    std::cout << "!!!! Host memory allocation error (matrixC)\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  /**
+   * Switch Case - To Initialize and Print input matrices based on mode passed,
+   * A, B and C are general matrices
+   */
+  
+  switch (mode) {
+    case 'S': {
+      util::InitializeMatrix<float>((float *)HostMatrixA, A_row, A_col);
+      util::InitializeMatrix<float>((float *)HostMatrixB, B_row, B_col);
+      util::InitializeMatrix<float>((float *)HostMatrixC, C_row, C_col);
+
+      std::cout << "\nMatrix A:\n";
+      util::PrintMatrix<float>((float *)HostMatrixA, A_row, A_col);
+      std::cout << "\nMatrix B:\n";
+      util::PrintMatrix<float>((float *)HostMatrixB, B_row, B_col);
+      std::cout << "\nMatrix C:\n";
+      util::PrintMatrix<float>((float *)HostMatrixC, C_row, C_col);
+          
+      break;
+    }
+
+    case 'C': {
+      util::InitializeComplexMatrix<cuComplex>((cuComplex *)HostMatrixA, A_row, A_col);
+      util::InitializeComplexMatrix<cuComplex>((cuComplex *)HostMatrixB, B_row, B_col);
+      util::InitializeComplexMatrix<cuComplex>((cuComplex *)HostMatrixC, C_row, C_col);
+
+      std::cout << "\nMatrix A:\n";
+      util::PrintComplexMatrix<cuComplex>((cuComplex *)HostMatrixA, A_row, A_col);
+      std::cout << "\nMatrix B:\n";
+      util::PrintComplexMatrix<cuComplex>((cuComplex *)HostMatrixB, B_row, B_col);
+      std::cout << "\nMatrix C:\n";
+      util::PrintComplexMatrix<cuComplex>((cuComplex *)HostMatrixC, C_row, C_col);
+      
+      break;
+    }
+  }
+  
+  //! Allocating Device Memory for Matrices using cudaMalloc()
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixA, A_row * A_col * sizeof(*HostMatrixA));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for A " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixB, B_row * B_col * sizeof(*HostMatrixB));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for B " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  cudaStatus = cudaMalloc((void **)&DeviceMatrixC, C_row * C_col * sizeof(*HostMatrixC));
+  if(cudaStatus != cudaSuccess) {
+    std::cout << " The device memory allocation failed for C " << std::endl;
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  //! Initializing CUBLAS context
+  status = cublasCreate(&handle);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "!!!! Failed to initialize handle\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  
+  //! Copying values of Host matrices to Device matrices using cublasSetMatrix()
+
+  status = cublasSetMatrix(A_row, A_col, sizeof(*HostMatrixA), HostMatrixA, A_row, DeviceMatrixA, A_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "Copying matrix A from host to device failed\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  status = cublasSetMatrix(B_row, B_col, sizeof(*HostMatrixB), HostMatrixB, B_row, DeviceMatrixB, B_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "Copying matrix B from host to device failed\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  status = cublasSetMatrix(C_row, C_col, sizeof(*HostMatrixC), HostMatrixC, C_row, DeviceMatrixC, C_row);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "Copying matrix C from host to device failed\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+  
+  /**
+   * API call to performs matrix - matrix multiplication : \f$ C = alpha * A * B + beta * C \f$
+   * This function is an extension of cublas<t>gemm
+   * In this function the input matrices and output matrices can have a lower precision but the computation is still done in the type <t>.
+   */
+    
+  /**
+   * The Error values returned by API are : \n
+   * CUBLAS_STATUS_SUCCESS - The operation completed successfully \n
+   * CUBLAS_STATUS_NOT_INITIALIZED - The library was not initialized \n
+   * CUBLAS_STATUS_ARCH_MISMATCH - cublasCgemmEx is only supported for GPU with architecture capabilities equal or greater than 5.0
+   * CUBLAS_STATUS_NOT_SUPPORTED - The combination of the parameters Atype,Btype and Ctype is not supported
+   * CUBLAS_STATUS_INVALID_VALUE - The parameters m, n, k < 0 \n
+   * CUBLAS_STATUS_ARCH_MISMATCH - In the case of cublasHgemmEx the device does not support math in half precision. \n
+   * CUBLAS_STATUS_EXECUTION_FAILED - The function failed to launch on the GPU \n
+   */
+  
+  switch (mode) {
+    case 'S': {
+      std::cout << "\nCalling SgemmEx API\n";
+      clk_start = clock();
+
+      status = cublasSgemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row,
+                           B_col, A_col, (float *)&alpha,
+                           (float *)DeviceMatrixA, CUDA_R_32F, A_row,
+                           (float *)DeviceMatrixB, CUDA_R_32F, B_row, (float *)&beta,
+                           (float *)DeviceMatrixC, CUDA_R_32F, C_row);
+
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        std::cout << "!!!!  SgemmEx kernel execution error\n";
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "SgemmEx API call ended\n";
+      break;
+    }
+
+    case 'C': {
+      std::cout << "\nCalling CgemmEx API\n";
+      clk_start = clock();
+
+      status = cublasCgemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row,
+                           B_col, A_col, (cuComplex *)&alpha,
+                           (cuComplex *)DeviceMatrixA, CUDA_C_32F, A_row,
+                           (cuComplex *)DeviceMatrixB, CUDA_C_32F, B_row,
+                           (cuComplex *)&beta,
+                           (cuComplex *)DeviceMatrixC, CUDA_C_32F, C_row);
+
+      if (status != CUBLAS_STATUS_SUCCESS) {
+        std::cout << "!!!!  CgemmEx kernel execution error\n";
+        FreeMemory();
+        return EXIT_FAILURE;
+      }
+
+      clk_end = clock();
+      std::cout << "CgemmEx API call ended\n";
+      break;
+    }
+  }
+  
+  //! Copy Matrix C, holding resultant matrix, from Device to Host using cublasGetMatrix()
+  status = cublasGetMatrix(C_row, C_col, sizeof(*HostMatrixC),
+                           DeviceMatrixC, C_row, HostMatrixC, C_row);
+
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "!!!! Unable to get output matrix C from device\n";
+    FreeMemory();
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "\nMatrix C after " << mode << "gemmEx operation is:\n";
+
+  switch (mode) {
+    case 'S': {  
+      util::PrintMatrix<float>((float *)HostMatrixC, C_row, C_col);
+      break;
+    }
+
+    case 'C': {
+      util::PrintComplexMatrix<cuComplex>((cuComplex *)HostMatrixC, C_row ,C_col);
+      break;
+    }
+  }
+
+  long long total_operations = A_row * A_col * B_col;
+
+  //! printing latency and throughput of the function
+  std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
+               "\nThroughput: " << THROUGHPUT(clk_start, clk_end, total_operations) << "\n\n";
+
+  FreeMemory();
+
+  return EXIT_SUCCESS;
+}
+
+int mode_S(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+            
+  float alpha = (float)alpha_real;
+  float beta = (float)beta_real;
+
+  gemmEx<float> SgemmEx(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'S' );
+  return SgemmEx.gemmExApiCall();
+}
+
+int mode_C(int A_row, int A_col, int B_row, int B_col, int C_row, int C_col, double alpha_real, double alpha_imaginary,
+            double beta_real, double beta_imaginary) {
+            
+  cuComplex alpha = {(float)alpha_real, (float)alpha_imaginary};
+  cuComplex beta = {(float)beta_real, (float)beta_imaginary};
+
+  gemmEx<cuComplex> CgemmEx(A_row, A_col, B_row, B_col, C_row, C_col, alpha, beta, 'C');
+  return CgemmEx.gemmExApiCall();
+
+}
+
+
+int (*cublas_func_ptr[])(int, int, int, int, int, int, double, double, double, double) = {
+  mode_S, mode_C
+};
+
+int main(int argc, char **argv) {
+
+  int A_row, A_col, B_row, B_col, C_row, C_col, status;
+  double alpha_real, alpha_imaginary, beta_real, beta_imaginary;
+  char mode;
+    
+  std::unordered_map<char, int> mode_index;
+  mode_index['S'] = 0;
+  mode_index['C'] = 1;
+
+
+  std::cout << "\n\n" << argv[0] << std::endl;
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::cout << argv[loop_count] << " ";
+    if (loop_count + 1 < argc)
+      std::cout << argv[loop_count + 1] << std::endl;
+  }
+  std::cout << std::endl;
+
+  //! reading cmd line arguments and initializing the required parameters
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::string cmd_argument(argv[loop_count]);
+
+    if (!(cmd_argument.compare("-A_row")))
+      A_row = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-A_column")))
+      A_col = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-B_column")))
+      B_col = atoi(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-alpha_real")))
+      alpha_real = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-alpha_imaginary")))
+      alpha_imaginary = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-beta_real")))
+      beta_real = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-beta_imaginary")))
+      beta_imaginary = std::stod(argv[loop_count + 1]);
+
+    else if (!(cmd_argument.compare("-mode")))
+      mode = *(argv[loop_count + 1]);
+  }
+
+  //! Dimension check
+  if (A_row <= 0 || A_col <= 0 || B_col <= 0) {
+    std::cout << "Minimum dimension error\n";
+    return EXIT_FAILURE;
+  }
+  
+  //! initializing values for matrix B and C
+  B_row = A_col;
+  C_row = A_row;
+  C_col = B_col;
+
+  status = (*cublas_func_ptr[mode_index[mode]])(A_row, A_col, B_row, B_col,   C_row, C_col, alpha_real, 
+                                                alpha_imaginary, beta_real,   beta_imaginary);
+  
+  return status;
+}
